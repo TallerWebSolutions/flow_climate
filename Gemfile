80d4060cf115cf568342349d796f1ce498c9e8b6
Celso Martins <celso.av.martins@gmail.com>
2018-01-11 14:27:10 -0200

    basic environment configuration

---
+++ Gemfile
@@ -0,0 +1,45 @@
+# frozen_string_literal: true
+
+source 'https://rubygems.org'
+
+ruby '2.4.3'
+
+gem 'coffee-rails'
+gem 'devise'
+gem 'figaro'
+gem 'jquery-rails'
+gem 'pg', '~> 0.20'
+gem 'rails', '5.1.4'
+gem 'sidekiq'
+gem 'therubyracer', platforms: :ruby
+gem 'uglifier'
+gem 'yui-compressor'
+
+group :test, :development do
+  gem 'brakeman'
+  gem 'database_cleaner'
+  gem 'fabrication'
+  gem 'faker'
+  gem 'parser'
+  gem 'rspec-collection_matchers'
+  gem 'rspec-rails'
+  gem 'rubocop'
+  gem 'shoulda-matchers'
+  gem 'simplecov', require: false
+end
+
+group :development do
+  gem 'annotate'
+  gem 'web-console'
+  gem 'listen'
+end
+
+group :test do
+  gem 'rails-controller-testing'
+  gem 'rspec_junit_formatter'
+end
+
+group :production do
+  gem 'puma'
+  gem 'rails_12factor'
+end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,78 @@
+# frozen_string_literal: true
+
+ENV['RAILS_ENV'] ||= 'test'
+
+require 'simplecov'
+
+SimpleCov.start 'rails' do
+  minimum_coverage 100
+  add_group 'Repositories', 'app/repositories'
+  add_group 'Services', 'app/services'
+  add_group 'Uploaders', 'app/uploaders'
+end
+SimpleCov.command_name 'RSpec'
+
+require File.expand_path('../../config/environment', __FILE__)
+abort('The Rails environment is running in production mode!') if Rails.env.production?
+require 'rspec/rails'
+require 'shoulda/matchers'
+require 'rspec/collection_matchers'
+
+Dir[Rails.root.join('spec', 'support', '**', '*.rb')].each { |f| require f }
+
+Rails.logger.level = 4
+Devise.stretches = 1
+
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :rails
+  end
+end
+
+ActiveRecord::Migration.maintain_test_schema!
+
+RSpec.configure do |config|
+  config.order = :random
+  config.profile_examples = 10
+  config.fixture_path = Rails.root.join('spec', 'fixtures')
+  config.use_transactional_fixtures = true
+  config.infer_spec_type_from_file_location!
+  config.filter_rails_from_backtrace!
+  config.render_views
+
+  config.include Devise::Test::ControllerHelpers, type: :controller
+  config.include Devise::Test::ControllerHelpers, type: :view
+  config.include Warden::Test::Helpers
+  Warden.test_mode!
+
+  config.expect_with :rspec do |expectations|
+    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+  end
+
+  config.mock_with :rspec do |mocks|
+    mocks.verify_partial_doubles = true
+  end
+
+  config.shared_context_metadata_behavior = :apply_to_host_groups
+
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with :truncation, except: [ActiveRecord::InternalMetadata.table_name]
+  end
+
+  config.around(:each) do |example|
+    DatabaseCleaner.cleaning do
+      example.run
+    end
+  end
+
+  config.include ActiveJob::TestHelper
+  config.after(:each) do
+    clear_enqueued_jobs
+  end
+
+  config.after(:all) do
+    FileUtils.rm_rf(Dir[Rails.root.join('public', 'uploads', 'tmp', '[^.]*')]) if Rails.env.test?
+  end
+end
