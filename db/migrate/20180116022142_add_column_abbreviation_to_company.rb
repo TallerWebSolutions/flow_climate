82b3d8aa3386d3b3007424fbcb82e113a3c5e6fd
Celso Martins <celso.av.martins@gmail.com>
2018-01-16 01:38:20 -0200

    removed code to keep legacy data ok with not null field

--- db/migrate/20180116022142_add_column_abbreviation_to_company.rb
+++ db/migrate/20180116022142_add_column_abbreviation_to_company.rb
@@ -2,9 +2,6 @@
 
 class AddColumnAbbreviationToCompany < ActiveRecord::Migration[5.1]
   def change
-    add_column :companies, :abbreviation, :string, index: true
-    Company.find_by(name: 'Taller')&.update(name: 'Taller Negócios Digitais', abbreviation: 'taller')
-    Company.find_by(name: 'CIASC - Centro de Informática e Automação do Estado de Santa Catarina')&.update(abbreviation: 'CIASC')
-    change_column_null :companies, :abbreviation, false
+    add_column :companies, :abbreviation, :string, index: true, null: false
   end
 end
