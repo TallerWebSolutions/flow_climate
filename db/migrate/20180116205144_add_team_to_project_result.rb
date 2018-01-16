944c14f365ed5e9d78cf22f0fabb6b38974f86ed
Celso Martins <celso.av.martins@gmail.com>
2018-01-16 19:13:15 -0200

    removed fit to legacy data from migration

--- db/migrate/20180116205144_add_team_to_project_result.rb
+++ db/migrate/20180116205144_add_team_to_project_result.rb
@@ -2,12 +2,7 @@
 
 class AddTeamToProjectResult < ActiveRecord::Migration[5.1]
   def change
-    add_column :project_results, :team_id, :integer, index: true
-    titanium = Team.find_by(id: 5)
-    ProjectResult.all.each { |pr| pr.update(team_id: titanium.id) }
-
-    change_column_null :project_results, :team_id, false
-
+    add_column :project_results, :team_id, :integer, index: true, null: false
     add_foreign_key :project_results, :teams, column: :team_id
   end
 end
