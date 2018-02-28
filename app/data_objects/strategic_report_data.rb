869046e9ee336250b7b9ce7359ece7375691e232
Celso Martins <celso.av.martins@gmail.com>
2018-02-28 19:41:38 -0300

    strategic information to teams

--- app/data_objects/strategic_report_data.rb
+++ app/data_objects/strategic_report_data.rb
@@ -3,7 +3,7 @@
 class StrategicReportData
   attr_reader :array_of_months, :active_projects_count_data, :sold_hours_in_month, :consumed_hours_per_month, :available_hours_per_month, :flow_pressure_per_month_data, :money_per_month_data
 
-  def initialize(company)
+  def initialize(projects, total_available_hours)
     @array_of_months = []
     @active_projects_count_data = []
     @sold_hours_in_month = []
@@ -12,56 +12,56 @@ class StrategicReportData
     @flow_pressure_per_month_data = []
     @money_per_month_data = []
 
-    assign_months_by_projects_dates(company)
-    assign_active_projects_count_data(company)
-    assign_sold_hours_per_month(company)
-    assign_consumed_hours_per_month(company)
-    assign_available_hours_per_month(company)
-    assign_flow_pressure_per_month_data(company)
-    assign_money_per_month_data(company)
+    assign_months_by_projects_dates(projects)
+    assign_active_projects_count_data(projects)
+    assign_sold_hours_per_month(projects)
+    assign_consumed_hours_per_month(projects)
+    assign_available_hours_per_month(total_available_hours)
+    assign_flow_pressure_per_month_data(projects)
+    assign_money_per_month_data(projects)
   end
 
   private
 
-  def assign_consumed_hours_per_month(company)
+  def assign_consumed_hours_per_month(projects)
     @array_of_months.each do |month_year|
-      @consumed_hours_per_month << ProjectsRepository.instance.hours_consumed_per_month(company, Date.new(month_year[1], month_year[0], 1))
+      @consumed_hours_per_month << ProjectsRepository.instance.hours_consumed_per_month(projects, Date.new(month_year[1], month_year[0], 1))
     end
   end
 
-  def assign_available_hours_per_month(company)
+  def assign_available_hours_per_month(available_hours)
     @array_of_months.each do
-      @available_hours_per_month << company.total_available_hours
+      @available_hours_per_month << available_hours
     end
   end
 
-  def assign_sold_hours_per_month(company)
+  def assign_sold_hours_per_month(projects)
     @array_of_months.each do |month_year|
-      @sold_hours_in_month << ProjectsRepository.instance.active_projects_in_month(company, Date.new(month_year[1], month_year[0], 1)).sum(&:hours_per_month)
+      @sold_hours_in_month << ProjectsRepository.instance.active_projects_in_month(projects, Date.new(month_year[1], month_year[0], 1)).sum(&:hours_per_month)
     end
   end
 
-  def assign_active_projects_count_data(company)
+  def assign_active_projects_count_data(projects)
     @array_of_months.each do |month_year|
-      @active_projects_count_data << ProjectsRepository.instance.active_projects_in_month(company, Date.new(month_year[1], month_year[0], 1)).count
+      @active_projects_count_data << ProjectsRepository.instance.active_projects_in_month(projects, Date.new(month_year[1], month_year[0], 1)).count
     end
   end
 
-  def assign_flow_pressure_per_month_data(company)
+  def assign_flow_pressure_per_month_data(projects)
     @array_of_months.each do |month_year|
-      @flow_pressure_per_month_data << ProjectsRepository.instance.flow_pressure_to_month(company, Date.new(month_year[1], month_year[0], 1))
+      @flow_pressure_per_month_data << ProjectsRepository.instance.flow_pressure_to_month(projects, Date.new(month_year[1], month_year[0], 1))
     end
   end
 
-  def assign_money_per_month_data(company)
+  def assign_money_per_month_data(projects)
     @array_of_months.each do |month_year|
-      @money_per_month_data << ProjectsRepository.instance.money_to_month(company, Date.new(month_year[1], month_year[0], 1)).to_f
+      @money_per_month_data << ProjectsRepository.instance.money_to_month(projects, Date.new(month_year[1], month_year[0], 1)).to_f
     end
   end
 
-  def assign_months_by_projects_dates(company)
-    min_date = company.projects.running.minimum(:start_date)
-    max_date = company.projects.running.maximum(:end_date)
+  def assign_months_by_projects_dates(projects)
+    min_date = projects.running.minimum(:start_date)
+    max_date = projects.running.maximum(:end_date)
 
     return if max_date.blank? || min_date.blank?
 
