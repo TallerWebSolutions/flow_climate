fb490b8f5df76ea3723d873c95247d05abd7dc35
Celso Martins <celso.av.martins@gmail.com>
2018-03-21 21:34:31 -0300

    updated minor gems

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,7 +10,7 @@ class ApplicationController < ActionController::Base
   def not_found
     respond_to do |format|
       format.html { render file: Rails.root.join('public', '404'), layout: false, status: 404 }
-      format.js { render plain: '404 Not Found', status: 404 }
+      format.js { render plain: '404 Not Found', status: :not_found }
     end
   end
 end
