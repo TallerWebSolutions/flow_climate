9d5f96ee0679f265184bd59bcf053c58cfdbc3cc
Celso Martins <celso.av.martins@gmail.com>
2018-04-30 16:51:26 -0300

    assets precompiled

---
+++ public/assets/projects/charts-f9c5f3ff67f923affc9853dd2eea1d9e8b9c943f9e3049e0ba6afede856bbbc5.js
@@ -0,0 +1,19 @@
+$(function () {
+    var throughputDiv = $('#throughput-column');
+    buildColumnChart(throughputDiv);
+
+    var hoursPerDemandDiv = $('#hours-column');
+    buildColumnChart(hoursPerDemandDiv);
+
+    var burnupDemands = $('#burnup-demands');
+    buildBurnupChart(burnupDemands);
+
+    var flowPressureDiv = $('#flowpressure-column');
+    buildColumnChart(flowPressureDiv);
+
+    var cmdDiv = $('#cmd-column');
+    buildColumnChart(cmdDiv);
+
+    var leadtimeControlChart = $('#leadtime-control-chart');
+    buildScatterChart(leadtimeControlChart);
+});
