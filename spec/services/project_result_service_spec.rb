1e6d61dcc8d8215b39b95019db9b6bf35d473740
Celso Martins <celso.av.martins@gmail.com>
2018-05-15 13:33:20 -0300

    improved robustness in project_result_service

--- spec/services/project_result_service_spec.rb
+++ spec/services/project_result_service_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe ProjectResultService, type: :service do
     let!(:team_member) { Fabricate :team_member, team: team, total_monthly_payment: 100, hours_per_month: 40 }
     let!(:other_team_member) { Fabricate :team_member, team: team, total_monthly_payment: 300, hours_per_month: 50 }
     let(:project) { Fabricate :project, initial_scope: 10, start_date: 1.week.ago, end_date: 1.week.from_now }
-    let(:existent_project_result) { Fabricate :project_result }
+    let(:existent_project_result) { Fabricate :project_result, project: project }
     let(:other_existent_project_result) { Fabricate :project_result }
 
     context 'when the demand has no previous result' do
@@ -67,23 +67,44 @@ RSpec.describe ProjectResultService, type: :service do
       end
       context 'when the demand has a previous result' do
         context 'and the result is in another date' do
-          let!(:project_result) { Fabricate :project_result, team: team, project: project, result_date: Time.zone.today, cost_in_month: team.active_monthly_cost_for_billable_types([project.project_type]), available_hours: team.active_monthly_available_hours_for_billable_types([project.project_type]) }
+          let!(:first_project_result) { Fabricate :project_result, team: team, project: project, result_date: 4.days.ago }
+          let!(:previous_project_result) { Fabricate :project_result, team: team, project: project, result_date: 3.days.ago }
+          let!(:project_result) { Fabricate :project_result, team: team, project: project, result_date: 2.days.ago, cost_in_month: team.active_monthly_cost_for_billable_types([project.project_type]), available_hours: team.active_monthly_available_hours_for_billable_types([project.project_type]) }
+          let!(:after_project_result) { Fabricate :project_result, team: team, project: project, result_date: Time.zone.tomorrow, known_scope: 100 }
+
           context 'and has only the processed demand' do
-            let(:demand) { Fabricate :demand, project: project, project_result: project_result, created_date: 2.days.ago, end_date: nil }
+            let(:demand) { Fabricate :demand, project: project, project_result: previous_project_result, created_date: 1.day.ago, end_date: Time.zone.today }
 
             it 'changes the project result to the new date and deletes the previous result' do
               ProjectResultService.instance.compute_demand!(team, demand)
               new_project_result = demand.reload.project_result
               expect(new_project_result.id).not_to eq project_result.id
-              expect(new_project_result.result_date).to eq 2.days.ago.to_date
-              expect(ProjectResult.count).to eq 1
+              expect(new_project_result.result_date).to eq Time.zone.today
+              expect(new_project_result.demands).to eq [demand]
+              expect(new_project_result.qty_hours_downstream).to eq demand.effort_downstream.to_i
+              expect(new_project_result.qty_hours_upstream).to eq demand.effort_upstream.to_i
+              expect(new_project_result.known_scope).to eq 11
+
+              expect(ProjectResult.find_by(id: first_project_result.id)).to eq first_project_result
+
+              expect(ProjectResult.find_by(id: previous_project_result.id)).to be_nil
+
+              before_project_result = ProjectResult.find_by(id: project_result.id)
+              expect(before_project_result).to eq project_result
+              expect(before_project_result.known_scope).to eq 10
+
+              after_result_reload = ProjectResult.find_by(id: after_project_result.id)
+              expect(after_result_reload).to eq after_project_result
+              expect(after_result_reload.known_scope).to eq 11
+
+              expect(ProjectResult.count).to eq 4
             end
           end
 
-          context 'and has other demands' do
-            let!(:first_demand) { Fabricate :demand, project: project, project_result: project_result, created_date: 2.days.ago, end_date: nil }
-            let!(:second_demand) { Fabricate :demand, project: project, project_result: project_result, created_date: Time.zone.now, end_date: nil }
-            let!(:third_demand) { Fabricate :demand, project: project, project_result: project_result, created_date: Time.zone.now, end_date: nil }
+          context 'and the result has other demands' do
+            let!(:first_demand) { Fabricate :demand, project: project, project_result: previous_project_result, created_date: 1.day.ago, end_date: Time.zone.today }
+            let!(:second_demand) { Fabricate :demand, project: project, project_result: previous_project_result, created_date: 1.day.ago, end_date: nil }
+            let!(:third_demand) { Fabricate :demand, project: project, project_result: previous_project_result, created_date: 1.day.ago, end_date: nil }
 
             context 'and the new result does not exist' do
               it 'moves the demand to the new project result' do
@@ -93,18 +114,26 @@ RSpec.describe ProjectResultService, type: :service do
                 ProjectResultService.instance.compute_demand!(modified_team, first_demand)
 
                 new_project_result = first_demand.reload.project_result
-                expect(new_project_result.result_date).to eq 2.days.ago.to_date
+                expect(new_project_result.id).not_to eq project_result.id
+                expect(new_project_result.result_date).to eq Time.zone.today
                 expect(new_project_result.demands).to eq [first_demand]
-                expect(new_project_result.cost_in_month).to eq 300.0
-                expect(new_project_result.available_hours).to eq 50.0
+                expect(new_project_result.qty_hours_downstream).to eq first_demand.effort_downstream.to_i
+                expect(new_project_result.qty_hours_upstream).to eq first_demand.effort_upstream.to_i
+                expect(new_project_result.known_scope).to eq 13
 
-                updated_project_result = project_result.reload.reload
-                expect(updated_project_result.result_date).to eq Time.zone.today
-                expect(updated_project_result.cost_in_month).to eq 400.0
-                expect(updated_project_result.available_hours).to eq 90.0
-                expect(updated_project_result.demands).to match_array [second_demand, third_demand]
+                expect(ProjectResult.find_by(id: first_project_result.id)).to eq first_project_result
 
-                expect(ProjectResult.count).to eq 2
+                expect(previous_project_result.reload.demands).to match_array [second_demand, third_demand]
+
+                before_project_result = ProjectResult.find_by(id: project_result.id)
+                expect(before_project_result.demands).to eq []
+                expect(before_project_result.known_scope).to eq 10
+
+                after_result_reload = ProjectResult.find_by(id: after_project_result.id)
+                expect(after_result_reload).to eq after_project_result
+                expect(after_result_reload.known_scope).to eq 13
+
+                expect(ProjectResult.count).to eq 5
               end
             end
             context 'and the new result exists' do
@@ -114,14 +143,14 @@ RSpec.describe ProjectResultService, type: :service do
                 ProjectResultService.instance.compute_demand!(team, first_demand)
 
                 new_project_result = first_demand.reload.project_result
-                expect(new_project_result.result_date).to eq 2.days.ago.to_date
+                expect(new_project_result.result_date).to eq Time.zone.today
                 expect(new_project_result.demands).to eq [first_demand]
 
-                updated_project_result = project_result.reload.reload
-                expect(updated_project_result.result_date).to eq Time.zone.today
+                updated_project_result = previous_project_result.reload.reload
+                expect(updated_project_result.result_date).to eq 3.days.ago.to_date
                 expect(updated_project_result.demands).to match_array [second_demand, third_demand]
 
-                expect(ProjectResult.count).to eq 2
+                expect(ProjectResult.count).to eq 5
               end
             end
           end
