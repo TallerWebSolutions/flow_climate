b5e2438dcf9d517cf7e967cbec58bfd51295d69b
Celso Martins <celso.av.martins@gmail.com>
2018-06-15 15:05:32 -0300

    fixed the query to find the starting point to not consider negatives numbers

--- app/models/stage.rb
+++ app/models/stage.rb
@@ -60,7 +60,7 @@ class Stage < ApplicationRecord
   end
 
   def flow_start_point
-    company.stages.where(integration_pipe_id: integration_pipe_id).order(:order).first
+    company.stages.where(integration_pipe_id: integration_pipe_id).where('stages.order >= 0').order(:order).first
   end
 
   def inside_commitment_area?
