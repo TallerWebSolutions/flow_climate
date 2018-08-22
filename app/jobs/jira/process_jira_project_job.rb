061645b3e1234cf9bce97b2cb96d81703e944a44
Celso Martins <celso.av.martins@gmail.com>
2018-08-22 17:32:20 -0300

    fixed project sync with Jira

--- app/jobs/jira/process_jira_project_job.rb
+++ app/jobs/jira/process_jira_project_job.rb
@@ -3,12 +3,13 @@
 module Jira
   class ProcessJiraProjectJob < ApplicationJob
     def perform(jira_account, project_jira_key)
-      project = Jira::JiraApiService.new(jira_account).request_project(project_jira_key)
+      jira_project = Jira::JiraApiService.new(jira_account).request_project(project_jira_key)
 
-      project.issues.each do |jira_issue|
+      jira_project.issues.each do |jira_issue|
         next if jira_issue.attrs['key'].blank?
+        project_jira_config = Jira::ProjectJiraConfig.find_by(jira_project_key: jira_project.attrs['key'], jira_account_domain: jira_account.customer_domain)
         jira_issue_with_transitions = Jira::JiraApiService.new(jira_account).request_issue_details(jira_issue.attrs['key'])
-        Jira::JiraIssueAdapter.instance.process_issue!(jira_account, project, jira_issue_with_transitions)
+        Jira::JiraIssueAdapter.instance.process_issue!(jira_account, project_jira_config.project, jira_issue_with_transitions)
       end
     end
   end
--- spec/jobs/jira/process_jira_project_job_spec.rb
+++ spec/jobs/jira/process_jira_project_job_spec.rb
@@ -13,9 +13,12 @@ RSpec.describe Jira::ProcessJiraProjectJob, type: :active_job do
       let(:options) { { username: 'foo', password: 'bar', site: 'http://foo.bar', context_path: '/', auth_type: :basic, read_timeout: 120 } }
       let(:client) { JIRA::Client.new(options) }
 
-      let(:jira_project) { client.Project.build(key: 'FC', name: 'Example', projectTypeKey: 'business', lead: 'username') }
+      let(:jira_project) { client.Project.build({ key: 'FC', name: 'Example', projectTypeKey: 'business', lead: 'username' }.with_indifferent_access) }
       let!(:jira_issue) { client.Issue.build({ key: '10000', summary: 'foo of bar', fields: { created: '2018-07-02T11:20:18.998-0300', issuetype: { name: 'Story' }, customfield_10028: { value: 'Expedite' }, project: { key: 'FC' }, customfield_10024: [{ name: 'foo' }, { name: 'bar' }] }, changelog: { startAt: 0, maxResults: 2, total: 2, histories: [{ id: '10039', from: 'first_stage', to: 'second_stage', created: '2018-07-08T22:34:47.440-0300' }, { id: '10038', from: 'third_stage', to: 'first_stage', created: '2018-07-06T09:40:43.886-0300' }] } }.with_indifferent_access) }
-      let(:jira_account) { Fabricate :jira_account, base_uri: 'http://foo.bar', username: 'foo', password: 'bar' }
+      let(:jira_account) { Fabricate :jira_account, base_uri: 'http://foo.bar', username: 'foo', password: 'bar', customer_domain: 'xpto' }
+      let(:project) { Fabricate :project }
+      let!(:jira_config) { Fabricate :project_jira_config, project: project, jira_account_domain: jira_account.customer_domain, jira_project_key: 'FC' }
+
       let(:team) { Fabricate :team }
 
       context 'valid responses' do
@@ -25,7 +28,7 @@ RSpec.describe Jira::ProcessJiraProjectJob, type: :active_job do
               expect_any_instance_of(Jira::JiraApiService).to(receive(:request_project).with('FC') { jira_project })
               expect_any_instance_of(JIRA::Resource::Project).to(receive(:issues) { [jira_issue] })
               expect_any_instance_of(Jira::JiraApiService).to(receive(:request_issue_details).with('10000') { jira_issue })
-              expect(Jira::JiraIssueAdapter.instance).to receive(:process_issue!).with(jira_account, jira_project, jira_issue).once
+              expect(Jira::JiraIssueAdapter.instance).to receive(:process_issue!).with(jira_account, project, jira_issue).once
               Jira::ProcessJiraProjectJob.perform_now(jira_account, 'FC')
             end
           end
