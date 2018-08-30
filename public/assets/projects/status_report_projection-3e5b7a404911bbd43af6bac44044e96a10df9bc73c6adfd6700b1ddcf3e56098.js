d346793b6200b4cc7d777d6a56c30ea0a7a6c17f
Celso Martins <celso.av.martins@gmail.com>
2018-08-30 12:45:31 -0300

    cleaned the assets

--- public/assets/projects/status_report_projection-3e5b7a404911bbd43af6bac44044e96a10df9bc73c6adfd6700b1ddcf3e56098.js
+++
@@ -1,7 +0,0 @@
-$(function () {
-    var statusReportDatesOddsDiv = $('#status-report-dates-odds-column');
-    buildColumnChart(statusReportDatesOddsDiv);
-
-    var statusReportMonteCarloDiv = $('#status-report-montecarlo-column');
-    buildColumnChart(statusReportMonteCarloDiv);
-});
