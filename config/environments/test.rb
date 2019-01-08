587c3abe89893f2985f065e5d5549a5b6dd76581
Celso Martins <celso.av.martins@gmail.com>
2019-01-08 18:56:51 -0200

    forced :en locale to Faker

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -41,4 +41,6 @@ Rails.application.configure do
   config.action_view.raise_on_missing_translations = true
 
   config.action_mailer.default_url_options = { host: 'http://127.0.0.1', port: 3000 }
+
+  Faker::Config.locale = :en
 end
