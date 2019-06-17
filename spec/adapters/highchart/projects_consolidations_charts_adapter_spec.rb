7182888fa193d2d719c9dcc95e209ecbe03db29f
Celso Martins <celso.av.martins@gmail.com>
2019-06-17 19:44:19 -0300

    fixed stats tab in team show

--- spec/adapters/highchart/projects_consolidations_charts_adapter_spec.rb
+++ spec/adapters/highchart/projects_consolidations_charts_adapter_spec.rb
@@ -11,24 +11,25 @@ RSpec.describe Highchart::ProjectsConsolidationsChartsAdapter, type: :service do
 
     context 'having projects' do
       let!(:first_project) { Fabricate :project, customers: [customer], status: :maintenance, start_date: 3.months.ago, end_date: 2.months.ago, qty_hours: 1000, initial_scope: 95, value: 200.0 }
-      let!(:second_project) { Fabricate :project, customers: [customer], status: :executing, start_date: 3.months.ago, end_date: 1.month.ago, qty_hours: 500, initial_scope: 40, value: 3_453_220.0 }
+      let!(:second_project) { Fabricate :project, customers: [customer], status: :executing, start_date: 3.months.ago, end_date: Time.zone.today, qty_hours: 500, initial_scope: 40, value: 3_453_220.0 }
 
       let!(:project_consolidation) { Fabricate :project_consolidation, consolidation_date: 65.days.ago, project: first_project }
       let!(:other_project_consolidation) { Fabricate :project_consolidation, consolidation_date: 34.days.ago, project: second_project }
+      let!(:fields_nil_project_consolidation) { Fabricate :project_consolidation, consolidation_date: 34.days.ago, project: first_project, lead_time_p25: nil, lead_time_histogram_bin_min: nil, lead_time_max: nil, lead_time_min: nil }
 
       context 'passing no date filter' do
         it 'builds the data structure for scope_data_evolution' do
-          consolidation_data = Highchart::ProjectsConsolidationsChartsAdapter.new(ProjectConsolidation.all, first_project.start_date, first_project.end_date)
+          consolidation_data = Highchart::ProjectsConsolidationsChartsAdapter.new(ProjectConsolidation.all, first_project.start_date, second_project.end_date)
 
-          expect(consolidation_data.lead_time_data_range_evolution[:x_axis]).to eq [65.days.ago.to_date]
+          expect(consolidation_data.lead_time_data_range_evolution[:x_axis]).to eq [Date.new(2017, 12, 17), Date.new(2018, 1, 17)]
           expect(consolidation_data.lead_time_data_range_evolution[:y_axis][0][:name]).to eq I18n.t('charts.lead_time_data_range_evolution.total_range')
-          expect(consolidation_data.lead_time_data_range_evolution[:y_axis][0][:data]).to eq [1.3888888888888888e-05]
+          expect(consolidation_data.lead_time_data_range_evolution[:y_axis][0][:data]).to eq [1.3888888888888888e-05, 1.3888888888888888e-05]
 
           expect(consolidation_data.lead_time_data_range_evolution[:y_axis][1][:name]).to eq I18n.t('charts.lead_time_data_range_evolution.total_range_max')
-          expect(consolidation_data.lead_time_data_range_evolution[:y_axis][1][:data]).to eq [2.4305555555555558e-05]
+          expect(consolidation_data.lead_time_data_range_evolution[:y_axis][1][:data]).to eq [2.4305555555555558e-05, 2.4305555555555558e-05]
 
           expect(consolidation_data.lead_time_data_range_evolution[:y_axis][2][:name]).to eq I18n.t('charts.lead_time_data_range_evolution.total_range_min')
-          expect(consolidation_data.lead_time_data_range_evolution[:y_axis][2][:data]).to eq [2.5462962962962965e-05]
+          expect(consolidation_data.lead_time_data_range_evolution[:y_axis][2][:data]).to eq [2.5462962962962965e-05, 2.5462962962962965e-05]
         end
       end
     end
