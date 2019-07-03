afbfa39e82a2d33a9777e8ddb737926a54fbfc69
Celso Martins <celso.av.martins@gmail.com>
2019-07-03 14:55:28 -0300

    added required to addressable in webhooks controller

--- app/controllers/webhook_integrations_controller.rb
+++ app/controllers/webhook_integrations_controller.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require 'addressable/uri'
+
 class WebhookIntegrationsController < ApplicationController
   skip_before_action :verify_authenticity_token
   before_action :check_request
