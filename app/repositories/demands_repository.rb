c15134f44c73bf1fb1f892863dce4415d6db6f68
Celso Martins <celso.av.martins@gmail.com>
2019-07-31 19:38:06 -0300

    check boxes in team member form following what is in model

--- app/repositories/demands_repository.rb
+++ app/repositories/demands_repository.rb
@@ -23,8 +23,8 @@ class DemandsRepository
     demands_stories_to_projects(projects).to_end_dates(start_period, end_period)
   end
 
-  def throughput_to_products_and_period(products, start_period, end_period)
-    Demand.kept.story.where(product_id: products).to_end_dates(start_period, end_period)
+  def throughput_to_products_and_period(products, team, start_period, end_period)
+    Demand.kept.story.where(product_id: products, team: team).to_end_dates(start_period, end_period)
   end
 
   def created_to_projects_and_period(projects, start_period, end_period)
