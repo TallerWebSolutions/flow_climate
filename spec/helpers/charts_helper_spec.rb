7efb7fe77aaf050449103cab34011eeb78b35e2e
Celso Martins <celso.av.martins@gmail.com>
2019-10-10 15:25:23 -0300

    updated minor gems and improved spec to chart helper

--- spec/helpers/charts_helper_spec.rb
+++ spec/helpers/charts_helper_spec.rb
@@ -9,7 +9,7 @@ RSpec.describe ChartsHelper, type: :helper do
     let!(:project) { Fabricate :project, name: 'foo' }
     let!(:other_project) { Fabricate :project, name: 'bar' }
 
-    it { expect(helper.gantt_chart_to_projects('bla', Project.all)).to eq [{ data: [{ completed: 0.0, end: 1_575_946_800_000, name: 'foo', start: 1_565_406_000_000 }, { completed: 0.0, end: 1_575_946_800_000, name: 'bar', start: 1_565_406_000_000 }], name: 'bla' }] }
+    it { expect(helper.gantt_chart_to_projects('bla', Project.all)).to eq [{ data: [{ completed: 0.0, start: project.start_date.to_time.to_i * 1000, end: project.end_date.to_time.to_i * 1000, name: 'foo' }, { completed: 0.0, start: other_project.start_date.to_time.to_i * 1000, end: other_project.end_date.to_time.to_i * 1000, name: 'bar' }], name: 'bla' }] }
     it { expect(helper.gantt_chart_to_projects('bla', Project.none)).to eq [{ data: [], name: 'bla' }] }
   end
 end
