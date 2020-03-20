10f1df23fe21141b04086cff1a7f5dcade9cf70e
Celso Martins <celso.av.martins@gmail.com>
2020-03-20 12:56:18 -0300

    working on the intermitent specs

--- spec/jobs/companies_bulletim_job_spec.rb
+++ spec/jobs/companies_bulletim_job_spec.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 RSpec.describe CompaniesBulletimJob, type: :active_job do
+  before { travel_to Time.zone.local(2020, 3, 20, 10, 0, 0) }
+
+  after { travel_back }
+
   describe '.perform_later' do
     it 'enqueues after calling perform_later' do
       described_class.perform_later
@@ -17,10 +21,10 @@ RSpec.describe CompaniesBulletimJob, type: :active_job do
 
     let(:customer) { Fabricate :customer, company: company }
 
-    let!(:first_project) { Fabricate :project, company: company, customers: [customer], start_date: Time.zone.today }
-    let!(:second_project) { Fabricate :project, company: company, customers: [customer], start_date: Time.zone.today }
-    let!(:third_project) { Fabricate :project, company: company, customers: [customer], end_date: Time.zone.today }
-    let!(:fourth_project) { Fabricate :project, company: company, customers: [customer], end_date: Time.zone.today }
+    let!(:first_project) { Fabricate :project, company: company, customers: [customer], start_date: Time.zone.today, end_date: 2.weeks.from_now }
+    let!(:second_project) { Fabricate :project, company: company, customers: [customer], start_date: Time.zone.today, end_date: 4.weeks.from_now }
+    let!(:third_project) { Fabricate :project, company: company, customers: [customer], start_date: 1.month.ago, end_date: Time.zone.today }
+    let!(:fourth_project) { Fabricate :project, company: company, customers: [customer], start_date: 2.days.ago, end_date: Time.zone.today }
 
     it 'calls the mailer to send the data' do
       allow(Time.zone).to receive(:today).and_return Time.zone.today.beginning_of_week + 1.day
