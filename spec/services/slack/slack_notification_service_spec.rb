17e6fee49fb11ca5295a0ee1d0ed8df0fdc06309
Celso Martins <celso.av.martins@gmail.com>
2020-05-20 17:04:00 -0300

    trying to fix intermitent specs

--- spec/services/slack/slack_notification_service_spec.rb
+++ spec/services/slack/slack_notification_service_spec.rb
@@ -78,10 +78,11 @@ RSpec.describe Slack::SlackNotificationService, type: :service do
 
     describe '#notify_failure_load' do
       it 'calls slack notification method' do
-        expect(first_slack_notifier).to receive(:ping).with(I18n.t('slack_configurations.notifications.failure_load', team_name: team.name, failure_load: number_to_percentage(team.failure_load, precision: 2))).once
-        expect(first_slack_notifier).to receive(:ping).with(I18n.t('slack_configurations.notifications.project_failure_load', team_name: team.name, project_name: project.name, failure_load: number_to_percentage(project.failure_load, precision: 2))).once
+        slack_notifier = instance_double('Slack::SlackNotifier')
+        expect(slack_notifier).to receive(:ping).with(I18n.t('slack_configurations.notifications.failure_load', team_name: team.name, failure_load: number_to_percentage(team.failure_load, precision: 2))).once
+        expect(slack_notifier).to receive(:ping).with(I18n.t('slack_configurations.notifications.project_failure_load', team_name: team.name, project_name: project.name, failure_load: number_to_percentage(project.failure_load, precision: 2))).once
 
-        described_class.instance.notify_failure_load(first_slack_notifier, team)
+        described_class.instance.notify_failure_load(slack_notifier, team)
       end
     end
   end
