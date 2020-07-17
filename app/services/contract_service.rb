6ab6bc7d92c128a5b1360b32b8d9bd6a0753da43
Celso Martins <celso.av.martins@gmail.com>
2020-07-17 13:27:55 -0300

    only demands with no contract updates when contract was saved

--- app/services/contract_service.rb
+++ app/services/contract_service.rb
@@ -4,7 +4,7 @@ class ContractService
   include Singleton
 
   def update_demands(contract)
-    contract_demands = contract.customer.demands.to_dates(contract.start_date, contract.end_date)
+    contract_demands = contract.customer.demands.where(contract: nil).to_dates(contract.start_date, contract.end_date)
 
     jira_account = contract.company.jira_accounts.first
 
