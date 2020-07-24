a5fd3339185c70fe225a2596698976326cd3f2e6
Celso Martins <celso.av.martins@gmail.com>
2020-07-24 09:31:48 -0300

    updated minor gems

--- app/repositories/project_risk_alerts_repository.rb
+++ app/repositories/project_risk_alerts_repository.rb
@@ -6,7 +6,7 @@ class ProjectRiskAlertsRepository
   def group_projects_risk_colors(projects, risk_type)
     ProjectRiskAlert.joins(:project_risk_config)
                     .where('project_risk_alerts.created_at = (SELECT MAX(created_at) FROM project_risk_alerts inner_alerts WHERE inner_alerts.project_id = project_risk_alerts.project_id AND inner_alerts.project_risk_config_id = project_risk_alerts.project_risk_config_id)')
-                    .where(project_id: projects.pluck(:id))
+                    .where(project_id: projects.map(&:id))
                     .where(project_risk_configs: { risk_type: risk_type })
                     .order(Arel.sql('project_risk_alerts.alert_color'))
                     .group(:alert_color).count
