a91dc03a6f32524c2610af8a10bc47e0de640e0e
Celso Martins <celso.av.martins@gmail.com>
2020-09-28 16:05:48 -0300

    moved JiraIntegrationError to the Jira namespace

--- spec/models/jira_api_error_spec.rb
+++ spec/models/jira_api_error_spec.rb
@@ -1,6 +1,6 @@
 # frozen-string-literal: true
 
-RSpec.describe JiraApiError, type: :model do
+RSpec.describe Jira::JiraApiError, type: :model do
   context 'associations' do
     it { is_expected.to belong_to :demand }
   end
