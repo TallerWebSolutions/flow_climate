306324e52ff75bb64f5890e57b3b294b28f261c9
Celso Martins <celso.av.martins@gmail.com>
2021-08-09 17:09:02 -0300

    fixed effort computation

--- app/models/demand_block.rb
+++ app/models/demand_block.rb
@@ -95,6 +95,10 @@ class DemandBlock < ApplicationRecord
     demand.stage_at(unblock_time)
   end
 
+  def transition_when_blocked
+    demand.demand_transitions_at(block_time).first
+  end
+
   private
 
   def update_computed_fields
