ba8d8dc2b98553f283a80bcd3f77a959135790c8
Luiz Henrique Guerra <lhguerra@gmail.com>
2021-11-29 19:59:17 -0300

    feat: add leadtime charts to member dashboard page

--- app/controllers/team_members_controller.rb
+++ app/controllers/team_members_controller.rb
@@ -9,6 +9,12 @@ class TeamMembersController < AuthenticatedController
   def show
     assign_team_member_objects
     @member_demands = @team_member.demands
+    @member_finished_demands = @member_demands.finished_with_leadtime
+    statistics_service = Stats::StatisticsService.instance
+    demands_leadtimes = @member_finished_demands.map(&:leadtime)
+    @member_leadtime65 = statistics_service.percentile(65, demands_leadtimes)
+    @member_leadtime80 = statistics_service.percentile(80, demands_leadtimes)
+    @member_leadtime95 = statistics_service.percentile(95, demands_leadtimes)
     build_member_charts(@team_member)
 
     render :show
