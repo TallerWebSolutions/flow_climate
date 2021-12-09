97d66481dc395f48d7c50942d6159df1f1091392
Celso Martins <celso.av.martins@gmail.com>
2021-12-09 19:01:29 -0300

    changed graphql controller to authenticated

--- app/controllers/graphql_controller.rb
+++ app/controllers/graphql_controller.rb
@@ -1,10 +1,6 @@
 # frozen_string_literal: true
 
-class GraphqlController < ApplicationController
-  # If accessing from outside this domain, nullify the session
-  # This allows for outside API access while preventing CSRF attacks,
-  # but you'll have to authenticate your user separately
-  protect_from_forgery with: :null_session
+class GraphqlController < AuthenticatedController
 
   def execute
     query = params[:query]
