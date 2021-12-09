fc428e238a0eb05a1e8cdd50e1838cbe521f077c
Celso Martins <celso.av.martins@gmail.com>
2021-12-09 19:01:29 -0300

    changed graphql controller to authenticated

--- app/controllers/graphql_controller.rb
+++ app/controllers/graphql_controller.rb
@@ -1,11 +1,13 @@
 # frozen_string_literal: true
 
 class GraphqlController < AuthenticatedController
+
   def execute
     query = params[:query]
     operation_name = params[:operationName]
     context = {
-      current_user: current_user
+      # Query context goes here, for example:
+      # current_user: current_user,
     }
     result = FlowClimateSchema.execute(query, variables: params[:variables], context: context, operation_name: operation_name)
     render json: result
