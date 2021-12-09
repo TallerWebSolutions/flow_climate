0f83a8897f1ffb7e03a3c7eeee7547d62308976c
Luiz Henrique Guerra <lhguerra@gmail.com>
2021-12-09 19:12:40 -0300

    chore: add current user to graphql controller

--- app/controllers/graphql_controller.rb
+++ app/controllers/graphql_controller.rb
@@ -6,8 +6,7 @@ class GraphqlController < AuthenticatedController
     query = params[:query]
     operation_name = params[:operationName]
     context = {
-      # Query context goes here, for example:
-      # current_user: current_user,
+      current_user: current_user,
     }
     result = FlowClimateSchema.execute(query, variables: params[:variables], context: context, operation_name: operation_name)
     render json: result
