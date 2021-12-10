5f9fb2c1e9c13f24b290b7bb1126d5da281780e0
Celso Martins <celso.av.martins@gmail.com>
2021-12-10 10:56:47 -0300

    authorization to the graphql controller

--- config/initializers/cors.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-allow do
-  origins 'http://localhost:3000'
-
-  resource '*',
-           headers: :any,
-           methods: [:get, :post, :put, :patch, :delete, :options, :head]
-end
--- spec/controllers/graphql_controller_spec.rb
+++ spec/controllers/graphql_controller_spec.rb
@@ -1,37 +1,53 @@
 # frozen_string_literal: true
 
 RSpec.describe GraphqlController, type: :controller do
-  context 'with a valid team' do
-    it 'returns the query result' do
-      team = Fabricate :team
+  context 'unauthenticated' do
+    describe 'POST #execute' do
+      before { post :execute, params: { format: :json, query: '' } }
 
-      query =
-        %(query {
+      it { expect(response).to have_http_status :unauthorized }
+    end
+  end
+
+  context 'authenticated' do
+    let(:user) { Fabricate :user }
+
+    before { sign_in user }
+
+    describe 'POST #execute' do
+      context 'with a valid team' do
+        it 'returns the query result' do
+          team = Fabricate :team
+
+          query =
+            %(query {
         team(id: #{team.id}) {
           id
         }
       })
 
-      post :execute, params: { format: :json, query: query }
+          post :execute, params: { format: :json, query: query }
 
-      expect(response).to have_http_status :ok
-      expect(JSON.parse(response.body)).to eq({ 'data' => { 'team' => { 'id' => team.id.to_s } } })
-    end
-  end
+          expect(response).to have_http_status :ok
+          expect(JSON.parse(response.body)).to eq({ 'data' => { 'team' => { 'id' => team.id.to_s } } })
+        end
+      end
 
-  context 'with an inexistent team' do
-    it 'returns 404 with the correct message' do
-      query =
-        %(query {
+      context 'with an inexistent team' do
+        it 'returns 404 with the correct message' do
+          query =
+            %(query {
         team(id: 43) {
           id
         }
       })
 
-      post :execute, params: { format: :json, query: query }
+          post :execute, params: { format: :json, query: query }
 
-      expect(response).to have_http_status :not_found
-      expect(response.message).to eq('Not Found')
+          expect(response).to have_http_status :not_found
+          expect(response.message).to eq('Not Found')
+        end
+      end
     end
   end
 end
