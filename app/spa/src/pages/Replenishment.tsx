20c1d87dec82c100a6c478cf5db2b24b8819221c
Luiz Henrique Guerra <lhguerra@gmail.com>
2021-12-13 19:36:02 -0300

    feat: add dynamic project data pro replenishment projects table componente

--- app/spa/src/pages/Replenishment.tsx
+++ app/spa/src/pages/Replenishment.tsx
@@ -15,6 +15,12 @@ const QUERY = gql`
     }
   }
 `
+// throughputData
+// averageThroughput
+// increasedAvgThroughtput
+// leadTime
+// increasedLeadtime80
+// workInProgress
 
 const Replenishment = () => {
   const { data, loading, error } = useQuery(QUERY)
