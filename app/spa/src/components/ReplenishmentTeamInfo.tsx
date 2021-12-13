5728e4a84e7e7aa83b620d7fd0a11669f4c38474
Luiz Henrique Guerra <lhguerra@gmail.com>
2021-12-13 17:41:32 -0300

    chore: add replenishiment projects table

--- app/spa/src/components/ReplenishmentTeamInfo.tsx
+++ app/spa/src/components/ReplenishmentTeamInfo.tsx
@@ -1,50 +1,66 @@
 import { Card, CardContent, Typography, Grid } from "@mui/material"
 
-const ReplenishmentTeamInfo = (props: any) => {
-  return (
-      <Grid container spacing={2} justifyContent="space-around">
-        <Grid item>
-          <Card>
-            <CardContent>
-              <Typography variant="h6" component="h5">
-                Últimos Throughputs
-              </Typography>
-              <Typography>{ props['teamProps']['team']['teamThroughputData'].join(', ') }</Typography>
-            </CardContent>
-          </Card>
-        </Grid>
-        <Grid item>
-          <Card>
-            <CardContent>
-              <Typography variant="h6" component="h5">
-                Th Médio 4 Semanas
-              </Typography>
-              <Typography>{ props['teamProps']['team']['averageTeamThroughput'] }</Typography>
-            </CardContent>
-          </Card>
-        </Grid>
-        <Grid item>
-          <Card>
-            <CardContent>
-              <Typography variant="h6" component="h5">
-                Lead Time 4 semanas
-              </Typography>
-              <Typography>{ props['teamProps']['team']['teamLeadTime'].toFixed(3) }</Typography>
-            </CardContent>
-          </Card>
-        </Grid>
-        <Grid item>
-          <Card>
-            <CardContent>
-              <Typography variant="h6" component="h5">
-                Limite de WIP
-              </Typography>
-              <Typography>{ props['teamProps']['team']['teamWip'] }</Typography>
-            </CardContent>
-          </Card>
-        </Grid>
-      </Grid>
-  )
+type ComparativeValue = {
+  value: Number
+  increased: Boolean
 }
+
+type TeamReplenishment = {
+  throughputData: Number[]
+  averageThroughput: ComparativeValue
+  leadTime: ComparativeValue
+  wip: Number
+}
+
+type ReplenishmentTeamInfoProps = {
+  team: TeamReplenishment
+}
+
+const ReplenishmentTeamInfo = ({ team }: ReplenishmentTeamInfoProps) => (
+  <Grid container spacing={2} justifyContent="space-around">
+    <Grid item>
+      <Card>
+        <CardContent>
+          <Typography variant="h6" component="h5">
+            Últimos Throughputs
+          </Typography>
+          {team.throughputData.join(", ")}
+          <Typography></Typography>
+        </CardContent>
+      </Card>
+    </Grid>
+    <Grid item>
+      <Card>
+        <CardContent>
+          <Typography variant="h6" component="h5">
+            Th Médio 4 Semanas
+            <Typography>{team.averageThroughput.value}</Typography>
+          </Typography>
+        </CardContent>
+      </Card>
+    </Grid>
+    <Grid item>
+      <Card>
+        <CardContent>
+          <Typography variant="h6" component="h5">
+            Lead Time 4 semanas
+          </Typography>
+          {team.leadTime.value.toFixed(3)}
+          <Typography></Typography>
+        </CardContent>
+      </Card>
+    </Grid>
+    <Grid item>
+      <Card>
+        <CardContent>
+          <Typography variant="h6" component="h5">
+            Limite de WIP
+            <Typography>{team.wip}</Typography>
+          </Typography>
+        </CardContent>
+      </Card>
+    </Grid>
+  </Grid>
+)
 
 export default ReplenishmentTeamInfo
