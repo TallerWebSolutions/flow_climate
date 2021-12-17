87cb794e94a67db7c45400e6dad0effa0a9824ed
Celso Martins <celso.av.martins@gmail.com>
2021-12-17 15:33:43 -0300

    fs: added avatar to the header

--- app/spa/src/components/Header.tsx
+++ app/spa/src/components/Header.tsx
@@ -1,38 +1,32 @@
-import { Box, Container, Link } from "@mui/material"
+import {Avatar, Box, Container, Link} from "@mui/material";
 
-const buildLinks = (companyName: string) => [
-  { name: "Taller", href: `/companies/${companyName}` },
-  { name: "Clientes", href: `/companies/${companyName}/customers` },
-  { name: "Produtos", href: `/companies/${companyName}/products` },
-  { name: "Projetos", href: `/companies/${companyName}/projects` },
-  { name: "Demandas", href: `/companies/${companyName}/demands` },
-  { name: "Bloqueios", href: `/companies/${companyName}/demand_blocks` },
-  { name: "Eventos", href: `/companies/${companyName}/flow_events` },
-]
+import {toImplementMessage} from "../pages/Replenishment";
 
-const Header = ({ companyName }: { companyName: string }) => (
-  <Box bgcolor="primary.dark" py={1}>
-    <Container>
-      <Box display="flex" alignItems="center">
-        <img
-          src="https://res.cloudinary.com/taller-digital/image/upload/v1599220860/2_taller_branco_horizontal.png"
-          alt="Taller Logo"
-          height={64}
-        />
-        {buildLinks(companyName).map((link, index) => (
-          <Link
-            sx={{ textDecoration: "none" }}
-            px={2}
-            key={link.name + index}
-            href={link.href}
-            color="primary.contrastText"
-          >
-            {link.name}
-          </Link>
-        ))}
-      </Box>
-    </Container>
-  </Box>
-)
+const buildLinks = (companyName: string) => ([
+  {name: 'Taller', href: `/companies/${companyName}`},
+  {name: 'Clientes', href: `/companies/${companyName}/customers`},
+  {name: 'Produtos', href: `/companies/${companyName}/products`},
+  {name: 'Projetos', href: `/companies/${companyName}/projects`},
+  {name: 'Demandas', href: `/companies/${companyName}/demands`},
+  {name: 'Bloqueios', href: `/companies/${companyName}/demand_blocks`},
+  {name: 'Eventos', href: `/companies/${companyName}/flow_events`},
+])
+
+type User = {
+  avatarSource: string,
+  fullName: string,
+}
+
+const Header = ({companyName, user}: {companyName: string, user: User}) => <Box bgcolor="primary.dark" py={1}>
+  <Container>
+    <Box display='flex' alignItems="center" justifyContent="space-between">
+      <img src="https://res.cloudinary.com/taller-digital/image/upload/v1599220860/2_taller_branco_horizontal.png" alt="Taller Logo" height={64} />
+      {buildLinks(companyName).map((link, index) => (
+        <Link sx={{textDecoration: 'none'}} px={2} key={link.name + index} href={link.href} color="primary.contrastText">{link.name}</Link>
+      ))}
+      <Avatar alt={user.fullName} src={user.avatarSource} onClick={toImplementMessage}/>
+    </Box>
+  </Container>
+</Box>
 
 export default Header
