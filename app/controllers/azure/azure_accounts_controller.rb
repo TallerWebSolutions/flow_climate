2698c2e93800cd9e45e396a60ac57a7daf2677ad
Celso Martins <celso.av.martins@gmail.com>
2022-01-21 20:13:55 -0300

    chore: updated minor gems

--- app/controllers/azure/azure_accounts_controller.rb
+++ app/controllers/azure/azure_accounts_controller.rb
@@ -6,7 +6,7 @@ module Azure
 
     def synchronize_azure
       azure_account = @company.azure_account
-      Azure::AzureSyncJob.perform_now(azure_account, current_user.email, current_user.full_name)
+      Azure::AzureSyncJob.perform_later(azure_account, current_user.email, current_user.full_name)
       flash[:notice] = I18n.t('general.enqueued')
       redirect_to company_path(@company)
     end
