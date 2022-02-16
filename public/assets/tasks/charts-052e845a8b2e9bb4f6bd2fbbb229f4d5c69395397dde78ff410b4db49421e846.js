03bde91dd3a921e4aed83558704ff06fcbdd92a8
Celso Martins <celso.av.martins@gmail.com>
2022-02-15 23:59:38 -0300

    chore: precompiled assets

---
+++ public/assets/tasks/charts-052e845a8b2e9bb4f6bd2fbbb229f4d5c69395397dde78ff410b4db49421e846.js
@@ -0,0 +1,14 @@
+const tasksBasedScatterPlot = $("#tasks-completion-scatter");
+if (tasksBasedScatterPlot.length !== 0) {
+    buildScatterChart(tasksBasedScatterPlot);
+}
+
+const tasksListThroughput = $("#tasks-list-throughput-data-column");
+if (tasksListThroughput.length !== 0) {
+    buildColumnChart(tasksListThroughput);
+}
+
+const completionTimeEvolution = $("#tasks-list-completion-time-evolution");
+if (completionTimeEvolution.length !== 0) {
+    buildLineChart(completionTimeEvolution);
+};
--- public/assets/tasks/charts-6cc8b94ebd08033f3d7a1eebaedcd75118d3cae2465759e59807d7524d29e8b1.js
+++
@@ -1,4 +0,0 @@
-const projectTaskBasedScatterPlot = $("#project-dashboard-task-completion-scatter");
-if (projectTaskBasedScatterPlot.length !== 0) {
-    buildScatterChart(projectTaskBasedScatterPlot);
-};
