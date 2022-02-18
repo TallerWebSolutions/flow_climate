13c3d1bf03bfd9e2d2fca81cb6f8fb201bbf1c8b
Celso Martins <celso.av.martins@gmail.com>
2022-02-18 20:09:40 -0300

    test: fixed spec covergage

--- app/jobs/jira/process_jira_project_job.rb
+++ app/jobs/jira/process_jira_project_job.rb
@@ -9,14 +9,18 @@ module Jira
       jira_product_key = jira_project_config.jira_product_config.jira_product_key
       project_issues = Jira::JiraApiService.new(jira_account.username, jira_account.api_token, jira_account.base_uri).request_issues_by_fix_version(jira_product_key, jira_project_config.fix_version_name)
 
+      processed_keys = []
       project_issues.each do |jira_issue|
         jira_issue_key = jira_issue.attrs['key']
         next if jira_issue_key.blank?
 
         Jira::ProcessJiraIssueJob.perform_later(jira_account, jira_project_config.project, jira_issue_key, nil, nil, nil)
+        processed_keys << jira_issue_key
       end
 
-      jira_project_config.project.demands.each do |demand|
+      demands_not_processed = jira_project_config.project.demands.map(&:external_id) - processed_keys
+
+      jira_project_config.project.demands.where(external_id: demands_not_processed).each do |demand|
         Jira::ProcessJiraIssueJob.perform_later(jira_account, jira_project_config.project, demand.external_id, nil, nil, nil)
       end
 
--- spec/jobs/jira/process_jira_project_job_spec.rb
+++ spec/jobs/jira/process_jira_project_job_spec.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 RSpec.describe Jira::ProcessJiraProjectJob, type: :active_job do
+  let(:company) { Fabricate :company }
+
   describe '.perform_later' do
     it 'enqueues after calling perform_later' do
       described_class.perform_later(bla: 'foo')
@@ -9,15 +11,16 @@ RSpec.describe Jira::ProcessJiraProjectJob, type: :active_job do
   end
 
   describe '.perform' do
-    context 'having params' do
+    context 'with params' do
       let(:options) { { username: 'foo', password: 'bar', site: 'http://foo.bar', context_path: '/', auth_type: :basic } }
       let(:client) { JIRA::Client.new(options) }
 
       let(:jira_project) { client.Project.build({ key: 'FC', name: 'Example', projectTypeKey: 'business', lead: 'username' }.with_indifferent_access) }
       let!(:jira_issue) { client.Issue.build({ key: '10000', summary: 'foo of bar', fields: { created: '2018-07-02T11:20:18.998-0300', issuetype: { name: 'Story' }, customfield_10028: { value: 'Expedite' }, project: { key: 'FC' }, customfield_10024: [{ name: 'foo' }, { name: 'bar' }] }, changelog: { startAt: 0, maxResults: 2, total: 2, histories: [{ id: '10039', from: 'first_stage', to: 'second_stage', created: '2018-07-08T22:34:47.440-0300' }, { id: '10038', from: 'third_stage', to: 'first_stage', created: '2018-07-06T09:40:43.886-0300' }] } }.with_indifferent_access) }
       let(:jira_account) { Fabricate :jira_account, base_uri: 'http://foo.bar', username: 'foo', api_token: 'bar', customer_domain: 'xpto' }
-      let(:product) { Fabricate :product }
+      let(:product) { Fabricate :product, company: company }
       let(:project) { Fabricate :project, products: [product] }
+      let!(:demand) { Fabricate :demand, company: company, project: project }
       let!(:jira_product_config) { Fabricate :jira_product_config, product: product, jira_product_key: 'FC' }
 
       let!(:jira_config) { Fabricate :jira_project_config, jira_product_config: jira_product_config, project: project }
@@ -30,7 +33,7 @@ RSpec.describe Jira::ProcessJiraProjectJob, type: :active_job do
             it 'calls the adapter to translation' do
               allow_any_instance_of(Jira::JiraApiService).to(receive(:request_issues_by_fix_version) { [jira_issue] })
               allow_any_instance_of(Jira::JiraApiService).to(receive(:request_issue_changelog).with('10000') { jira_issue })
-              expect(Jira::ProcessJiraIssueJob).to receive(:perform_later).once
+              expect(Jira::ProcessJiraIssueJob).to receive(:perform_later).twice
               described_class.perform_now(jira_account, jira_config, 'foo@bar.com', 'Foo Bar', 'http://foo.com.br')
             end
           end
