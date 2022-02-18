2e14d3d057f4707abd8f996d29c6888cc7e4276c
Celso Martins <celso.av.martins@gmail.com>
2022-02-18 12:41:21 -0300

    chore: assets precompiled

---
+++ public/assets/team_members/user_dashboard_operations_charts-6f304596509f78f070822e2234f62ceb3ee9d82fa726ad93afd35e90f3d53331.js
@@ -0,0 +1,36 @@
+function buildOperationsCharts() {
+  const lineLeadtimeAccumulated = $("#line-leadtime-accumalated-user");
+  if (lineLeadtimeAccumulated.length !== 0) {
+    buildLineChart(lineLeadtimeAccumulated);
+  }
+
+  const linePullInterval = $("#member-dashboard-pull-interval-line");
+  if (linePullInterval.length !== 0) {
+    buildLineChart(linePullInterval);
+  }
+
+  const userEffortDiv = $("#member-dashboard-effort-column");
+  if (userEffortDiv.length !== 0) {
+    buildColumnChart(userEffortDiv);
+  }
+
+  const teamMemberThroughputDiv = $("#member-dashboard-throughput-column");
+  if (teamMemberThroughputDiv.length !== 0) {
+    buildColumnChart(teamMemberThroughputDiv);
+  }
+
+  const memberLeadTimeControlChart = $("#member-lead-time-control-chart");
+  if (memberLeadTimeControlChart.length !== 0) {
+    buildScatterChart(memberLeadTimeControlChart);
+  }
+
+  const memberLeadTimeHistogram = $("#member-lead-time-histogram");
+  if (memberLeadTimeHistogram.length !== 0) {
+    buildColumnChart(memberLeadTimeHistogram);
+  }
+
+  const teamMemberHoursPerProject = $("#member-dashboard-hours-per-project");
+  if (teamMemberHoursPerProject.length !== 0) {
+    buildColumnChart(teamMemberHoursPerProject);
+  }
+};
