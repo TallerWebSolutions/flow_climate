a8843dcb55942b15df46805bb9de6786ba400506
Anderson Oliveira <contactandersonoliveira@gmail.com>
2022-03-09 19:09:49 -0300

    feat: add secondsToReadbleDate on date helper

--- app/graphql/types/project_consolidation_type.rb
+++ app/graphql/types/project_consolidation_type.rb
@@ -10,6 +10,22 @@ module Types
     field :weeks_by_little_law, Float, null: false
     field :project, Types::ProjectType, null: false
     field :project_throughput, Float, null: false
+    field :lead_time_min, Int, null: true
+    field :lead_time_max, Int, null: true
+    field :lead_time_p80, Int, null: true
+    field :lead_time_std_dev, Int, null: true
+    field :lead_time_histogram_bin_min, Int, null: true
+    field :lead_time_histogram_bin_max, Int, null: true
+    field :lead_time_average, Float, null: true
+    field :demands_finished_ids, [Int], null: false
+    field :lead_time_feature, Int, null: true
+    field :lead_time_bug, Int, null: true
+    field :lead_time_chore, Int, null: true
+    field :lead_time_standard, Int, null: true
+    field :lead_time_fixed_date, Int, null: true
+    field :lead_time_expedite, Int, null: true    
+
+    field :total, [Int], null: false
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
 
     delegate :project, to: :object
