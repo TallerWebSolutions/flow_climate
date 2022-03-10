173c21786f30f881ff63c88dc50918baa24fd5cb
Anderson Oliveira <contactandersonoliveira@gmail.com>
2022-03-10 10:42:11 -0300

    refactor: make date helper secondsToReadble format based on seconds quantity

--- app/spa/src/lib/date.ts
+++ app/spa/src/lib/date.ts
@@ -5,15 +5,30 @@ import {
 } from "date-fns"
 import { ptBR } from "date-fns/locale"
 
-export const secondsToReadbleDate = (ms: number) => {
-  const msIntoInterger = parseInt(ms.toString())
+const SECONDS_IN_A_DAY = 24 * 60 * 60
+const SECONDS_IN_MINUTES = 60
+
+export const secondsToReadbleDate = (seconds: number) => {
+  if (!seconds || seconds <= 0) return "0 segundos"
+
+  const secondsIntoInterger = parseInt(seconds.toString())
   const msDuration = intervalToDuration({
     start: 0,
-    end: secondsToMilliseconds(msIntoInterger),
+    end: secondsToMilliseconds(secondsIntoInterger),
   })
 
+  var dateFormat = ["days", "hours"]
+
+  if (secondsIntoInterger < SECONDS_IN_A_DAY) {
+    dateFormat.push("minutes")
+  }
+
+  if (secondsIntoInterger <= SECONDS_IN_MINUTES) {
+    dateFormat.push("seconds")
+  }
+
   return formatDuration(msDuration, {
-    format: ["days", "hours"],
+    format: dateFormat,
     locale: ptBR,
     delimiter: " e ",
   })
