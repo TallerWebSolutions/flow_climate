fb64e90158aa3bc2916345b0e3589c9fd0d821b9
Anderson Oliveira <contactandersonoliveira@gmail.com>
2022-03-15 13:48:20 -0300

    test: add capitalizeFirstLetter case test

--- app/spa/src/lib/__tests__/func.ts
+++ app/spa/src/lib/__tests__/func.ts
@@ -1,7 +1,13 @@
-import { formatLeadtime } from "../func"
+import { capitalizeFirstLetter, formatLeadtime } from "../func"
 
 describe("format leadtime", () => {
   it("should take leadtime data and format it to human readable info", () => {
     expect(formatLeadtime(339940.1788)).toEqual(3.93)
+  })
+})
+
+describe("format string", () => {
+  it("should make the first letter of string uppercase", () => {
+    expect(capitalizeFirstLetter("name")).toEqual("Name")
   })
 })
