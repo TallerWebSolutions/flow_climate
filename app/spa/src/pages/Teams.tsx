e76d02a24b91fd60cf7957046aa62b2f152eeb60
Anderson Oliveira <contactandersonoliveira@gmail.com>
2022-03-15 14:57:30 -0300

    feat: add team listing on spa

--- app/spa/src/pages/Teams.tsx
+++ app/spa/src/pages/Teams.tsx
@@ -1,17 +1,22 @@
 import { gql, useQuery } from "@apollo/client"
 import {
   Backdrop,
+  Box,
+  Button,
   CircularProgress,
+  Link,
   Table,
+  TableBody,
   TableCell,
   TableHead,
   TableRow,
   Typography,
 } from "@mui/material"
+import EditOutlinedIcon from "@mui/icons-material/EditOutlined"
+import DeleteOutlineOutlinedIcon from "@mui/icons-material/DeleteOutlineOutlined"
 import BasicPage from "../components/BasicPage"
 import { Team } from "./Replenishing"
 import { Company } from "../modules/company/company.types"
-import { ReactElement } from "react"
 import { capitalizeFirstLetter } from "../lib/func"
 
 export const TEAMS_QUERY = gql`
@@ -39,18 +44,6 @@ export type TeamsDTO = {
   me: LoggedUser
 }
 
-type TableItemProps = {
-  children: ReactElement | ReactElement[]
-}
-
-const TableItem = ({ children }: TableItemProps) => {
-  return (
-    <TableRow>
-      <TableCell>{children}</TableCell>
-    </TableRow>
-  )
-}
-
 const Teams = () => {
   const { data, loading } = useQuery<TeamsDTO>(TEAMS_QUERY)
 
@@ -64,6 +57,7 @@ const Teams = () => {
   const company = data?.me.currentCompany
   const companyName = company?.slug
   const companyUrl = `/companies/${company?.slug}`
+  const teams = data?.teams
 
   const breadcrumbsLinks = [
     { name: capitalizeFirstLetter(companyName!), url: companyUrl! },
@@ -73,22 +67,68 @@ const Teams = () => {
   ]
 
   return (
-    <BasicPage title={"Teams"} breadcrumbsLinks={breadcrumbsLinks}>
-      <Table>
-        <TableHead>
-          <TableCell>
-            <Typography>Times</Typography>
-          </TableCell>
-        </TableHead>
+    <BasicPage
+      title={"Teams"}
+      company={company}
+      breadcrumbsLinks={breadcrumbsLinks}
+    >
+      <Box sx={{ display: "flex", justifyContent: "center", mt: 3 }}>
+        <Table sx={{ flex: "0 0 50%" }}>
+          <TableHead>
+            <TableRow>
+              <TableCell>
+                <Typography color="text.primary" sx={{ fontSize: "1.25rem" }}>
+                  Times
+                </Typography>
+              </TableCell>
+            </TableRow>
+          </TableHead>
 
-        <TableRow>
-          <TableCell>Text</TableCell>
-        </TableRow>
+          <TableBody>
+            {teams?.map(({ name, id }, index) => {
+              const teamLinkBase = `/companies/taller/teams/${id}`
 
-        <TableRow>
-          <TableCell>Text</TableCell>
-        </TableRow>
-      </Table>
+              return (
+                <TableRow
+                  key={`${name}-${index}`}
+                  sx={{ display: "flex", justifyContent: "space-between" }}
+                >
+                  <TableCell>
+                    <Link
+                      href={teamLinkBase}
+                      sx={{ textDecorationColor: "info.dark" }}
+                    >
+                      <Typography color="info.dark">{name}</Typography>
+                    </Link>
+                  </TableCell>
+                  <TableCell
+                    size={"small"}
+                    sx={{ display: "inline-flex", whiteSpace: "nowrap" }}
+                  >
+                    <Box mr={1}>
+                      <Link href={`${teamLinkBase}/edit`}>
+                        <EditOutlinedIcon color={"primary"} />
+                      </Link>
+                    </Box>
+
+                    <Link href={`${teamLinkBase}/delete`}>
+                      <DeleteOutlineOutlinedIcon color={"primary"} />
+                    </Link>
+                  </TableCell>
+                </TableRow>
+              )
+            })}
+          </TableBody>
+        </Table>
+
+        <Button
+          href="/companies/taller/teams/new"
+          variant="contained"
+          sx={{ height: "35", textTransform: "uppercase", color: "primary" }}
+        >
+          Criar Time
+        </Button>
+      </Box>
     </BasicPage>
   )
 }
