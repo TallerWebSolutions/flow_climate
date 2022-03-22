5f8545a501ccb996e91a2434750ddabb8db0de33
Celso Martins <celso.av.martins@gmail.com>
2022-03-22 17:52:42 -0300

    feat: added initiatives, projects and teams info to the company type

--- app/graphql/types/company_type.rb
+++ app/graphql/types/company_type.rb
@@ -5,5 +5,8 @@ module Types
     field :id, ID, null: false
     field :name, String, null: false
     field :slug, String, null: false
+    field :initiatives, [Types::InitiativeType], null: false
+    field :projects, [Types::ProjectType], null: false
+    field :teams, [Types::TeamType], null: false
   end
 end
--- app/graphql/types/demand_type.rb
+++ app/graphql/types/demand_type.rb
@@ -3,8 +3,9 @@
 module Types
   class DemandType < Types::BaseObject
     field :id, ID, null: false
-    field :demand_title, String, null: false
+    field :company, Types::CompanyType, null: false
     field :team, Types::TeamType, null: false
     field :project, Types::ProjectType, null: false
+    field :demand_title, String, null: false
   end
 end
