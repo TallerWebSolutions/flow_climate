44881ba88194c2bed1255cbf644dbd78c1edd527
Celso Martins <celso.av.martins@gmail.com>
2022-03-22 17:57:02 -0300

    chore: put the tasks specific count to its own class

--- app/graphql/types/tasks_connection.rb
+++ app/graphql/types/tasks_connection.rb
@@ -1,13 +1,8 @@
 # frozen_string_literal: true
 
 module Types
-  class BaseConnection < Types::BaseObject
+  class TasksConnection < BaseConnection
     include GraphQL::Types::Relay::ConnectionBehaviors
-
-    field :total_count, Integer, null: false
-    def total_count
-      object.items&.count
-    end
 
     field :total_finished_tasks_count, Integer, null: false
     def total_finished_tasks_count
