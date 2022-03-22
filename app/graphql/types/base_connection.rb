6aa516cdfd62f8d06ab4cc58813c49001f52b153
Celso Martins <celso.av.martins@gmail.com>
2022-03-22 17:38:27 -0300

    feat: added total delivered count to tasks query

--- app/graphql/types/base_connection.rb
+++ app/graphql/types/base_connection.rb
@@ -5,9 +5,13 @@ module Types
     include GraphQL::Types::Relay::ConnectionBehaviors
 
     field :total_count, Integer, null: false
-
     def total_count
       object.items&.count
+    end
+
+    field :total_finished_tasks_count, Integer, null: false
+    def total_finished_tasks_count
+      object.items&.finished&.count
     end
   end
 end
