260090bda3430f6e41943d1b6f5389af9a720138
Anderson Oliveira <contactandersonoliveira@gmail.com>
2022-03-22 15:18:18 -0300

    feat: add task select filters

--- app/spa/src/pages/Tasks.tsx
+++ app/spa/src/pages/Tasks.tsx
@@ -12,17 +12,29 @@ import {
   TablePagination,
   TableRow,
   Typography,
+  TextField,
 } from "@mui/material"
-import { ChangeEvent, useState } from "react"
+import { ChangeEvent, useEffect, useState, useCallback } from "react"
 import { useTranslation } from "react-i18next"
-import { useParams } from "react-router-dom"
 import BasicPage from "../components/BasicPage"
 import { secondsToReadbleDate } from "../lib/date"
 import { Task } from "../modules/task/task.types"
+import { Team } from "../modules/team/team.types"
+import User from "../modules/user/user.types"
 
 const TASKS_QUERY = gql`
-  query Tasks($page: Int!, $limit: Int) {
-    tasks(pageParam: $page, limit: $limit) {
+  query TasksPage(
+    $pageParam: Int!
+    $limit: Int
+    $title: String
+    $status: String
+  ) {
+    tasks(
+      pageParam: $pageParam
+      limit: $limit
+      title: $title
+      status: $status
+    ) {
       id
       demand {
         id
@@ -33,11 +45,26 @@ const TASKS_QUERY = gql`
       secondsToComplete
       partialCompletionTime
     }
+
+    me {
+      language
+      currentCompany {
+        name
+        slug
+      }
+    }
+
+    teams {
+      id
+      name
+    }
   }
 `
 
 type TasksDTO = {
   tasks: Task[]
+  me: User
+  teams: Team[]
 }
 
 const normalizeTimeToFinish = (
@@ -53,16 +80,33 @@ const normalizeTimeToFinish = (
 
 const Tasks = () => {
   const { t } = useTranslation(["tasks"])
-  const { companyNickName } = useParams()
-  const [listPage, setListPage] = useState(1)
-  const [rowsPerPage, setRowsPerPage] = useState(10)
+  const [taskSearchName, setTaskSearchName] = useState("")
+  const [taskFilters, setTaskFilters] = useState({
+    pageParam: 1,
+    limit: 10,
+    title: "",
+    status: "not_finished",
+  })
 
   const { data, loading } = useQuery<TasksDTO>(TASKS_QUERY, {
-    variables: {
-      page: listPage,
-      limit: rowsPerPage,
-    },
+    variables: { ...taskFilters },
   })
+
+  useEffect(() => console.log({ ...taskFilters }), [taskFilters])
+
+  useEffect(() => {
+    const bounceTime = setTimeout(() => {
+      setTaskFilters((prevState) => ({ ...prevState, title: taskSearchName }))
+    }, 1000)
+
+    return () => clearTimeout(bounceTime)
+  }, [taskSearchName])
+
+  const handleSearchByName = useCallback(
+    (event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) =>
+      setTaskSearchName(String(event.target.value)),
+    [taskFilters]
+  )
 
   if (loading)
     return (
@@ -73,6 +117,9 @@ const Tasks = () => {
 
   const tasks = data?.tasks!
   const numberOfTasks = tasks.length
+  const company = data?.me.currentCompany!
+  const teams = data?.teams!
+
   const taskListHeadCells = [
     "ID",
     t("tasks_table.demand"),
@@ -81,21 +128,123 @@ const Tasks = () => {
     t("tasks_table.delivery_date"),
     t("tasks_table.time_to_finish"),
   ]
+  const breadcrumbsLinks = [
+    { name: company.name || "", url: company.slug },
+    { name: t("tasks") },
+  ]
 
   const handleRowsPerPage = (
     event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
   ) => {
     const quantity: number = Number(event.target.value)
-    setRowsPerPage(quantity)
+    setTaskFilters((prevState) => ({ ...prevState, limit: quantity }))
   }
 
-  const handleChangePage = (newPage: number) => {
-    setListPage(newPage)
+  const handlePage = (newPage: number) => {
+    setTaskFilters((prevState) => ({ ...prevState, pageParam: newPage }))
+  }
+
+  const handleStatus = (
+    event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    const status = event.target.value
+    setTaskFilters((prevState) => ({ ...prevState, status }))
+  }
+
+  const handleTeam = (
+    event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    const teamId = Number(event.target.value)
+    setTaskFilters((prevState) => ({ ...prevState, teamId }))
   }
 
   return (
-    <BasicPage title={t("tasks")} breadcrumbsLinks={[]}>
+    <BasicPage
+      title={t("tasks")}
+      company={company}
+      breadcrumbsLinks={breadcrumbsLinks}
+    >
       <Box>
+        <Box
+          sx={{
+            display: "grid",
+            gridTemplateColumns: "repeat(4, 1fr)",
+            gridTemplateRows: "repeat(2, 1fr)",
+            gridColumnGap: "16px",
+            gridRowGap: "20px",
+          }}
+        >
+          <TextField
+            value={taskSearchName}
+            onChange={handleSearchByName}
+            label={t("filter.search")}
+            helperText={t("filter.search_helper")}
+          />
+          <TextField placeholder={t("filter.initial_date")} type="date" />
+          <TextField label={t("filter.end_date")} type="date" />
+
+          <TextField
+            select
+            label={t("filter.status")}
+            value={taskFilters.status}
+            onChange={handleStatus}
+            SelectProps={{
+              native: true,
+            }}
+          >
+            <option value="not_finished">{t("filter.status_open")}</option>
+            <option value="finished">{t("filter.status_finished")}</option>
+          </TextField>
+
+          <TextField
+            select
+            label={t("filter.initiative")}
+            value={teams[0]}
+            onChange={() => {}}
+            SelectProps={{
+              native: true,
+            }}
+          >
+            {teams.map((team) => (
+              <option key={team.id} value={team.id}>
+                {team.name}
+              </option>
+            ))}
+          </TextField>
+
+          <TextField
+            select
+            label={t("filter.project")}
+            value={teams[0]}
+            onChange={() => {}}
+            SelectProps={{
+              native: true,
+            }}
+          >
+            {teams.map((team) => (
+              <option key={team.id} value={team.id}>
+                {team.name}
+              </option>
+            ))}
+          </TextField>
+
+          <TextField
+            select
+            label={t("filter.team")}
+            value={teams[0]}
+            onChange={handleTeam}
+            SelectProps={{
+              native: true,
+            }}
+          >
+            {teams.map((team) => (
+              <option key={team.id} value={team.id}>
+                {team.name}
+              </option>
+            ))}
+          </TextField>
+        </Box>
+
         <TableContainer>
           <Typography color="primary" variant="h6" component="h6">
             {`877 ${t("tasks")} - 405 ${t("finished_tasks")}`}
@@ -127,9 +276,7 @@ const Tasks = () => {
                   key={`${row.demand}--${index}`}
                 >
                   <TableCell padding="checkbox">
-                    <Link
-                      href={`/companies/${companyNickName}/tasks/${row.id}`}
-                    >
+                    <Link href={`/companies/${company.slug}/tasks/${row.id}`}>
                       {row.id}
                     </Link>
                   </TableCell>
@@ -160,9 +307,9 @@ const Tasks = () => {
             rowsPerPageOptions={[5, 10, 25]}
             component="div"
             count={numberOfTasks}
-            rowsPerPage={rowsPerPage}
-            page={listPage}
-            onPageChange={(_, page) => handleChangePage(page)}
+            rowsPerPage={taskFilters.limit}
+            page={taskFilters.pageParam}
+            onPageChange={(_, page) => handlePage(page)}
             onRowsPerPageChange={(event) => handleRowsPerPage(event)}
           />
         </TableContainer>
