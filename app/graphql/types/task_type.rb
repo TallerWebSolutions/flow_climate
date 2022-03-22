63f20f3d61bd4bb9bbc5e53d1a09ad795c8a143d
Celso Martins <celso.av.martins@gmail.com>
2022-03-22 17:38:27 -0300

    feat: added total delivered count to tasks query

--- app/graphql/types/task_type.rb
+++ app/graphql/types/task_type.rb
@@ -7,10 +7,15 @@ module Types
     field :initiative, Types::InitiativeType, null: false
     field :project, Types::ProjectType, null: false
     field :demand, Types::DemandType, null: false
+    field :delivered, Boolean, null: false
     field :title, String, null: false
     field :created_date, GraphQL::Types::ISO8601Date, null: false
     field :end_date, GraphQL::Types::ISO8601Date, null: true
     field :seconds_to_complete, Float, null: true
     field :partial_completion_time, Float, null: true
+
+    def delivered
+      object.end_date.present?
+    end
   end
 end
