6021fe0b11c5458e912496e3cb25037f991c056d
Anderson Oliveira <contactandersonoliveira@gmail.com>
2022-03-22 16:11:19 -0300

    wip

--- app/graphql/types/project_type.rb
+++ app/graphql/types/project_type.rb
@@ -5,6 +5,7 @@ module Types
   class ProjectType < Types::BaseObject
     field :id, ID, null: false
     field :company, Types::CompanyType, null: false
+    field :initiative, Types::InitiativeType, null: true
     field :name, String, null: false
     field :start_date, GraphQL::Types::ISO8601Date, null: false
     field :end_date, GraphQL::Types::ISO8601Date, null: false
