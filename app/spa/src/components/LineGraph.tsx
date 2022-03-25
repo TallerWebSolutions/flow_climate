bd237536fe0ab88fa1fc80b978d8e6a3e581f344
Anderson Oliveira <contactandersonoliveira@gmail.com>
2022-03-25 07:24:12 -0300

    feat: add LineGraph to components

---
+++ app/spa/src/components/LineGraph.tsx
@@ -0,0 +1,48 @@
+import { ResponsiveLine, Serie } from "@nivo/line"
+
+type LineGraphProps = {
+  data: Serie[]
+  axisLeftLegend: string
+}
+
+export const LineGraph = ({ data, axisLeftLegend }: LineGraphProps) => {
+  return (
+    <ResponsiveLine
+      data={data}
+      colors={{ scheme: "pastel2" }}
+      margin={{ left: 80, right: 20, top: 25, bottom: 40 }}
+      axisLeft={{
+        legend: axisLeftLegend,
+        legendOffset: -50,
+        legendPosition: "middle",
+      }}
+      useMesh={true}
+      legends={[
+        {
+          anchor: "top",
+          direction: "row",
+          justify: false,
+          translateX: 0,
+          translateY: -25,
+          itemsSpacing: 0,
+          itemDirection: "left-to-right",
+          itemWidth: 125,
+          itemHeight: 20,
+          itemOpacity: 0.75,
+          symbolSize: 12,
+          symbolShape: "circle",
+          symbolBorderColor: "rgba(0, 0, 0, .5)",
+          effects: [
+            {
+              on: "hover",
+              style: {
+                itemBackground: "rgba(0, 0, 0, .03)",
+                itemOpacity: 1,
+              },
+            },
+          ],
+        },
+      ]}
+    />
+  )
+}
--- app/spa/src/pages/Statistics.tsx
+++ app/spa/src/pages/Statistics.tsx
@@ -1,8 +1,8 @@
 import { gql, useQuery } from "@apollo/client"
 import { Backdrop, Box, CircularProgress, Typography } from "@mui/material"
-import { ResponsiveLine, Serie } from "@nivo/line"
 import { ReactElement } from "react"
 import { useParams } from "react-router-dom"
+import { LineGraph } from "../components/LineGraph"
 import {
   ProjectPage,
   PROJECT_STANDARD_FRAGMENT,
@@ -68,53 +68,6 @@ const GraphBox = ({ title, children }: GraphBoxProps) => {
       </Typography>
       {children}
     </Box>
-  )
-}
-
-type LineGraphProps = {
-  data: Serie[]
-  axisLeftLegend: string
-}
-
-const LineGraph = ({ data, axisLeftLegend }: LineGraphProps) => {
-  return (
-    <ResponsiveLine
-      data={data}
-      colors={{ scheme: "pastel2" }}
-      margin={{ left: 80, right: 20, top: 25, bottom: 40 }}
-      axisLeft={{
-        legend: axisLeftLegend,
-        legendOffset: -50,
-        legendPosition: "middle",
-      }}
-      useMesh={true}
-      legends={[
-        {
-          anchor: "top",
-          direction: "row",
-          justify: false,
-          translateX: 0,
-          translateY: -25,
-          itemsSpacing: 0,
-          itemDirection: "left-to-right",
-          itemWidth: 125,
-          itemHeight: 20,
-          itemOpacity: 0.75,
-          symbolSize: 12,
-          symbolShape: "circle",
-          symbolBorderColor: "rgba(0, 0, 0, .5)",
-          effects: [
-            {
-              on: "hover",
-              style: {
-                itemBackground: "rgba(0, 0, 0, .03)",
-                itemOpacity: 1,
-              },
-            },
-          ],
-        },
-      ]}
-    />
   )
 }
 
