423a0479d72ce055748919d18a3448e2f0fa1e08
Celso Martins <celso.av.martins@gmail.com>
2022-03-25 10:52:36 -0300

    feat: added lead time percentis to the tasks list query

--- app/data_objects/tasks_list.rb
+++ app/data_objects/tasks_list.rb
@@ -10,4 +10,24 @@ class TasksList
     @total_pages = total_pages
     @tasks = tasks
   end
+
+  def distribution_lead_time_p65
+    Stats::StatisticsService.instance.percentile(65, completion_times)
+  end
+
+  def distribution_lead_time_p80
+    Stats::StatisticsService.instance.percentile(80, completion_times)
+  end
+
+  def distribution_lead_time_p95
+    Stats::StatisticsService.instance.percentile(95, completion_times)
+  end
+
+  private
+
+  def completion_times
+    return [] if @tasks.blank?
+
+    @completion_times ||= @tasks.finished.map(&:seconds_to_complete)
+  end
 end
--- app/graphql/types/tasks_list_type.rb
+++ app/graphql/types/tasks_list_type.rb
@@ -8,5 +8,8 @@ module Types
     field :total_pages, Int, null: false
 
     field :tasks, [Types::TaskType], null: false
+    field :distribution_lead_time_p65, Float, null: false
+    field :distribution_lead_time_p80, Float, null: false
+    field :distribution_lead_time_p95, Float, null: false
   end
 end
