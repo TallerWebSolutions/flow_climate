7bce39a06ccbd8ee2867dae8c283e03d43ccdde3
Anderson Oliveira <contactandersonoliveira@gmail.com>
2022-04-07 13:18:46 -0300

    feat: add TabPanel to components

---
+++ app/spa/src/components/TabPanel.tsx
@@ -0,0 +1,29 @@
+import { Box, Typography } from "@mui/material"
+
+type TabPanelProps = {
+  children?: React.ReactNode
+  index: number
+  value: number
+}
+
+const TabPanel = (props: TabPanelProps) => {
+  const { children, value, index, ...other } = props
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  )
+}
+
+export default TabPanel
