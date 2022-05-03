6359f05eca19e9b433c1b2b17815c9c81e33f5fa
Eduardo Luiz <hello@eduardoluiz.dev>
2022-05-03 17:55:21 -0300

    chore: add margin to bottom axis items in bar chart

--- app/spa/src/components/charts/BarChart.tsx
+++ app/spa/src/components/charts/BarChart.tsx
@@ -1,5 +1,5 @@
 import { BarDatum, BarLegendProps, ResponsiveBar } from "@nivo/bar"
-import {Box} from "@mui/material"
+import { Box } from "@mui/material"
 import { ReactElement } from "react"
 import { BarData } from "./tooltips/BarChartTooltip"
 
@@ -10,73 +10,81 @@ type BarChartProps = {
   axisLeftLegend?: string
   axisBottomLegend?: string
   tooltip?: (data: BarData) => ReactElement
-  legendAnchor?: BarLegendProps['anchor']
-  legendDirection?: BarLegendProps['direction']
+  legendAnchor?: BarLegendProps["anchor"]
+  legendDirection?: BarLegendProps["direction"]
 }
 
-export const BarChart = ({ data, axisLeftLegend, axisBottomLegend, indexBy, keys, legendAnchor = "top", legendDirection = "row" }: BarChartProps) => (
-  <Box height={350}>
+export const BarChart = ({
+  data,
+  axisLeftLegend,
+  axisBottomLegend,
+  indexBy,
+  keys,
+  legendAnchor = "top",
+  legendDirection = "row",
+}: BarChartProps) => (
+  <Box height={380}>
     <ResponsiveBar
-    data={data}
-    indexBy={indexBy}
-    keys={keys}
-    margin={{ top: 50, right: 130, bottom: 50, left: 60 }}
-    padding={0.3}
-    colors={{ scheme: "nivo" }}
-    borderColor={{
-      from: "color",
-      modifiers: [["darker", 1.6]],
-    }}
-    axisBottom={{
-      tickSize: 5,
-      tickPadding: 5,
-      tickRotation: -40,
-      legendPosition: "middle",
-      legendOffset: 40,
-      legend: axisBottomLegend
-    }}
-    axisLeft={{
-      tickSize: 5,
-      tickPadding: 5,
-      tickRotation: 0,
-      legend: axisLeftLegend,
-      legendPosition: "middle",
-      legendOffset: -40,
-    }}
-    labelSkipWidth={12}
-    labelSkipHeight={12}
-    labelTextColor={{
-      from: "color",
-      modifiers: [["darker", 1.6]],
-    }}
-    legends={[
-      {
-        dataFrom: "keys",
-        anchor: legendAnchor,
-        direction: legendDirection,
-        toggleSerie: true,
-        justify: false,
-        translateX: 0,
-        translateY: -25,
-        itemsSpacing: 0,
-        itemDirection: "left-to-right",
-        itemWidth: 125,
-        itemHeight: 20,
-        itemOpacity: 0.75,
-        symbolSize: 12,
-        symbolShape: "circle",
-        symbolBorderColor: "rgba(0, 0, 0, .5)",
-        effects: [
-          {
-            on: "hover",
-            style: {
-              itemBackground: "rgba(0, 0, 0, .03)",
-              itemOpacity: 1,
+      data={data}
+      indexBy={indexBy}
+      keys={keys}
+      margin={{ top: 50, right: 130, bottom: 80, left: 60 }}
+      padding={0.3}
+      colors={{ scheme: "nivo" }}
+      borderColor={{
+        from: "color",
+        modifiers: [["darker", 1.6]],
+      }}
+      axisBottom={{
+        tickSize: 5,
+        tickPadding: 5,
+        tickRotation: -40,
+        legendPosition: "middle",
+        legendOffset: 40,
+        legend: axisBottomLegend,
+      }}
+      axisLeft={{
+        tickSize: 5,
+        tickPadding: 5,
+        tickRotation: 0,
+        legend: axisLeftLegend,
+        legendPosition: "middle",
+        legendOffset: -40,
+      }}
+      labelSkipWidth={12}
+      labelSkipHeight={12}
+      labelTextColor={{
+        from: "color",
+        modifiers: [["darker", 1.6]],
+      }}
+      legends={[
+        {
+          dataFrom: "keys",
+          anchor: legendAnchor,
+          direction: legendDirection,
+          toggleSerie: true,
+          justify: false,
+          translateX: 0,
+          translateY: -25,
+          itemsSpacing: 0,
+          itemDirection: "left-to-right",
+          itemWidth: 125,
+          itemHeight: 20,
+          itemOpacity: 0.75,
+          symbolSize: 12,
+          symbolShape: "circle",
+          symbolBorderColor: "rgba(0, 0, 0, .5)",
+          effects: [
+            {
+              on: "hover",
+              style: {
+                itemBackground: "rgba(0, 0, 0, .03)",
+                itemOpacity: 1,
+              },
             },
-          },
-        ],
-      },
-    ]}
-  />
+          ],
+        },
+      ]}
+    />
   </Box>
 )
