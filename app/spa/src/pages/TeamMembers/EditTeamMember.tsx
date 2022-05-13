32c0f509a6eb199872f75cc7b11b1ebf1a76cd84
Luiz Henrique Guerra <lhguerra@gmail.com>
2022-05-13 17:46:14 -0300

    feat: add default values to edit member form

--- app/spa/src/pages/TeamMembers/EditTeamMember.tsx
+++ app/spa/src/pages/TeamMembers/EditTeamMember.tsx
@@ -2,14 +2,53 @@ import { FormControl, FormGroup, Input, InputLabel } from "@mui/material"
 import { useContext } from "react"
 import { useTranslation } from "react-i18next"
 import { useForm } from "react-hook-form"
+import { useParams } from "react-router-dom"
+import { Backdrop, CircularProgress } from "@mui/material"
+import { gql, useQuery, useMutation } from "@apollo/client"
 
 import BasicPage from "../../components/BasicPage"
 import { MeContext } from "../../contexts/MeContext"
 
+const FORM_VALUES_QUERY = gql`
+  query EditTeamMemberFormValues($teamMemberId: Int!) {
+    teamMember(id: $teamMemberId) {
+      id
+      name
+      startDate
+      endDate
+      jiraAccountUserEmail
+      jiraAccountId
+      billable
+      hoursPerMonth
+      monthlyPayment
+      teams {
+        id
+        name
+      }
+    }
+  }
+`
+
 const EditTeamMember = () => {
   const { t } = useTranslation(["teamMembers"])
+  const { teamMemberId } = useParams()
   const { me } = useContext(MeContext)
   const { register, handleSubmit } = useForm()
+  const { data, loading } = useQuery(FORM_VALUES_QUERY, {
+    variables: {
+      teamMemberId: Number(teamMemberId),
+    },
+  })
+
+  if (loading)
+    return (
+      <Backdrop open>
+        <CircularProgress color="secondary" />
+      </Backdrop>
+    )
+
+  const teamMember = data?.teamMember
+  if (!teamMember) return <strong>{t("teamMembers.notFound")}</strong>
 
   const companyUrl = `/companies/${me?.currentCompany?.slug}`
   const breadcrumbsLinks = [
@@ -27,9 +66,54 @@ const EditTeamMember = () => {
     <BasicPage breadcrumbsLinks={breadcrumbsLinks} title={t("edit.title")}>
       <form>
         <FormGroup>
-          <FormControl>
+          <FormControl sx={{ marginBottom: 4 }}>
             <InputLabel htmlFor="name">{t("edit.form.name")}</InputLabel>
-            <Input {...register("name", { required: true })} />
+            <Input
+              {...register("name", { required: true })}
+              defaultValue={teamMember.name}
+            />
+          </FormControl>
+          <FormControl sx={{ marginBottom: 4 }}>
+            <InputLabel htmlFor="startDate" shrink>
+              {t("edit.form.startDate")}
+            </InputLabel>
+            <Input
+              {...register("startDate", { required: true })}
+              type="date"
+              defaultValue={teamMember.startDate}
+            />
+          </FormControl>
+          <FormControl sx={{ marginBottom: 4 }}>
+            <InputLabel htmlFor="endDate" shrink>
+              {t("edit.form.endDate")}
+            </InputLabel>
+            <Input
+              {...register("endDate", { required: true })}
+              type="date"
+              defaultValue={teamMember.endDate}
+            />
+          </FormControl>
+          <FormControl sx={{ marginBottom: 4 }}>
+            <InputLabel htmlFor="monthlyPayment">
+              {t("edit.form.monthlyPayment")}
+            </InputLabel>
+            <Input
+              {...register("monthlyPayment", { required: true })}
+              type="number"
+              inputProps={{ step: "any" }}
+              defaultValue={teamMember.monthlyPayment}
+            />
+          </FormControl>
+          <FormControl sx={{ marginBottom: 4 }}>
+            <InputLabel htmlFor="hoursPerMonth">
+              {t("edit.form.hoursPerMonth")}
+            </InputLabel>
+            <Input
+              {...register("hoursPerMonth", { required: true })}
+              type="number"
+              inputProps={{ step: "any" }}
+              defaultValue={teamMember.hoursPerMonth}
+            />
           </FormControl>
         </FormGroup>
       </form>
