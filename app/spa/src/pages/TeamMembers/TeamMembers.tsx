62ec2ef433f5fb5213e544193612bca8c2873685
Luiz Henrique Guerra <lhguerra@gmail.com>
2022-05-17 17:01:39 -0300

    fix status logic for team members

--- app/spa/src/pages/TeamMembers/TeamMembers.tsx
+++ app/spa/src/pages/TeamMembers/TeamMembers.tsx
@@ -81,8 +81,8 @@ const TeamMembers = () => {
       teamMember.endDate,
       teamMember.billable ? <CheckIcon color="primary" /> : "",
       teamMember.endDate
-        ? t("columns.status.active")
-        : t("columns.status.inactive"),
+        ? t("columns.status.inactive")
+        : t("columns.status.active"),
       <Link to={`${companyUrl}/team_members/${teamMember.id}/edit`}>
         <EditOutlinedIcon color="primary" />
       </Link>,
