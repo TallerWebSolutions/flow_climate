b0b5860366656dbb132810707e5368cb42070f84
Celso Martins <celso.av.martins@gmail.com>
2022-07-13 11:51:20 -0300

    feat: adapter already reading the new infos

--- app/adapters/azure/azure_work_item_adapter.rb
+++ app/adapters/azure/azure_work_item_adapter.rb
@@ -4,13 +4,6 @@ module Azure
   class AzureWorkItemAdapter < Azure::AzureAdapter
     attr_reader :project_custom_field, :team_custom_field
 
-    def initialize(azure_account)
-      super(azure_account)
-
-      @project_custom_field = azure_account.azure_custom_fields.find_by(custom_field_type: :project_name)
-      @team_custom_field = azure_account.azure_custom_fields.find_by(custom_field_type: :team_name)
-    end
-
     def work_items_ids(azure_product_config)
       work_items_ids = []
       items_ids_response = client.work_items_ids(azure_product_config)
@@ -27,8 +20,6 @@ module Azure
 
     def work_item(work_item_id, azure_project)
       company = @azure_account.company
-
-      return if project_custom_field.blank? || team_custom_field.blank?
 
       work_item_response = client.work_item(work_item_id, azure_project.project_id)
       return if work_item_response.blank?
@@ -49,11 +40,10 @@ module Azure
     end
 
     def process_valid_area(company, product, azure_project, work_item_response)
-      team_name = work_item_response['fields'][@team_custom_field.custom_field_name]
-      return if team_name.blank?
-
-      team = read_team(company, team_name)
-      project = read_project(company, team, work_item_response)
+      customer = Azure::AzureReader.instance.read_customer(company, work_item_response)
+      team = Azure::AzureReader.instance.read_team(company, @azure_account, work_item_response)
+      initiative = Azure::AzureReader.instance.read_initiative(company, work_item_response)
+      project = Azure::AzureReader.instance.read_project(company, customer, team, initiative, @azure_account, work_item_response)
 
       work_item_type = work_item_response['fields']['System.WorkItemType']
       if work_item_type.casecmp('epic').zero?
@@ -70,7 +60,7 @@ module Azure
 
       demand = Demand.with_discarded.where(external_id: work_item_response['id']).first_or_initialize
 
-      demand_type = read_card_type(project.company, work_item_response, :demand)
+      demand_type = AzureReader.instance.read_card_type(project.company, work_item_response, :demand)
 
       demand.update(company: company, team: team, demand_title: work_item_response['fields']['System.Title'].strip,
                     created_date: work_item_response['fields']['System.CreatedDate'],
@@ -87,7 +77,7 @@ module Azure
     def read_user_story(product, project, team, azure_project, work_item_response)
       demand = read_task_parent(product, project, team, azure_project, work_item_response)
 
-      task_type = read_card_type(project.company, work_item_response, :task)
+      task_type = AzureReader.instance.read_card_type(project.company, work_item_response, :task)
 
       task = Task.with_discarded.where(external_id: work_item_response['id']).first_or_initialize
       task.update(title: work_item_response['fields']['System.Title'], created_date: work_item_response['fields']['System.CreatedDate'],
@@ -95,14 +85,6 @@ module Azure
                   work_item_type: task_type, discarded_at: nil)
 
       task
-    end
-
-    def read_card_type(company, work_item_response, item_level)
-      type = company.work_item_types.where(name: work_item_response['fields']['System.Tags'], item_level: item_level).first_or_create
-
-      return type if type.valid?
-
-      company.work_item_types.where(name: 'Default', item_level: item_level).first_or_create
     end
 
     def read_task_parent(product, project, team, azure_project, work_item_response)
@@ -115,27 +97,6 @@ module Azure
       return if parent_response.blank? || parent_response.code != 200
 
       read_feature(product, project, team, parent_response.parsed_response)
-    end
-
-    def read_team(company, team_name)
-      team = company.teams.where('name ILIKE :team_name', team_name: "%#{team_name}%").first
-      team = company.teams.create(name: team_name) if team.blank?
-      team
-    end
-
-    def read_project(company, team, work_item_response)
-      project_name = work_item_response['fields'][@project_custom_field.custom_field_name]
-
-      project_name = 'Other' if project_name.blank?
-
-      project_name += " - #{team.name}"
-
-      project = company.projects.where(name: project_name, team: team).first_or_initialize
-      unless project.persisted?
-        project.update(qty_hours: 0, project_type: :outsourcing, status: :executing, start_date: Time.zone.today,
-                       end_date: 3.months.from_now, initial_scope: 0, value: 0, hour_value: 0)
-      end
-      project
     end
   end
 end
---
+++ app/readers/azure/azure_reader.rb
@@ -0,0 +1,60 @@
+# frozen_string_literal: true
+
+module Azure
+  class AzureReader
+    include Singleton
+
+    def read_team(company, azure_account, work_item_response)
+      team_custom_field = azure_account.azure_custom_fields.find_by(custom_field_type: :team_name)
+      team_name = work_item_response['fields'][team_custom_field.custom_field_name] || 'Default Team'
+      company.teams.where('name ILIKE :team_name', team_name: "%#{team_name}%").first_or_create
+    end
+
+    def read_customer(company, work_item_response)
+      customer_custom_field = 'Custom.Category'
+      customer_name = work_item_response['fields'][customer_custom_field] || 'Default Customer'
+      customer = company.customers.where('name ILIKE :customer_name', customer_name: "%#{customer_name}%").first
+      return company.customers.create(name: customer_name) if customer.blank?
+
+      customer
+    end
+
+    def read_initiative(company, work_item_response)
+      quarter_custom_field = 'Custom.TargetQuarter'
+      year_custom_field = 'Custom.Year'
+
+      quarter = work_item_response['fields'][quarter_custom_field]
+      year = work_item_response['fields'][year_custom_field]
+
+      return if quarter.blank? || year.blank?
+
+      company.initiatives.where('name ILIKE :team_name', team_name: "%#{quarter}/#{year}%").first_or_create
+    end
+
+    def read_card_type(company, work_item_response, item_level)
+      type = company.work_item_types.where(name: work_item_response['fields']['System.Tags'], item_level: item_level).first_or_create
+
+      return type if type.valid?
+
+      company.work_item_types.where(name: 'Default', item_level: item_level).first_or_create
+    end
+
+    def read_project(company, customer, team, initiative, azure_account, work_item_response)
+      project_custom_field = azure_account.azure_custom_fields.find_by(custom_field_type: :project_name)
+      project_name = work_item_response['fields'][project_custom_field.custom_field_name]
+
+      project_name = 'Other' if project_name.blank?
+
+      project_name += " - #{team.name}"
+
+      project = company.projects.where(name: project_name, team: team).first_or_initialize
+      unless project.persisted?
+        project.update(qty_hours: 0, project_type: :outsourcing, status: :executing, start_date: Time.zone.today,
+                       end_date: 3.months.from_now, initial_scope: 0, value: 0, hour_value: 0)
+      end
+
+      project.update(customers: [customer], initiative: initiative)
+      project
+    end
+  end
+end
