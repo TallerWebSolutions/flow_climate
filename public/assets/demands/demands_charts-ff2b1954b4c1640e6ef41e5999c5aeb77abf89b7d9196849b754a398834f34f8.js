fecdf5420b6b2e6b2af577ac054bedc597b71818
Celso Martins <celso.av.martins@gmail.com>
2022-08-05 18:45:03 -0300

    chore: assets precompiled

---
+++ public/assets/demands/demands_charts-ff2b1954b4c1640e6ef41e5999c5aeb77abf89b7d9196849b754a398834f34f8.js
@@ -0,0 +1,46 @@
+function buildDemandsTabCharts() {
+    const demandsThroughputDiv = $("#demands-throughput-column");
+    if (demandsThroughputDiv.length !== 0) {
+        buildColumnChart(demandsThroughputDiv);
+    }
+
+    const demandsCreatedDiv = $("#demands-created-column");
+    if (demandsCreatedDiv.length !== 0) {
+        buildColumnChart(demandsCreatedDiv);
+    }
+
+    const demandsCommittedDiv = $("#demands-committed-column");
+    if (demandsCommittedDiv.length !== 0) {
+        buildColumnChart(demandsCommittedDiv);
+    }
+
+    const leadtimeEvolution = $("#leadtime-evolution");
+    if (leadtimeEvolution.length !== 0) {
+        buildLineChart(leadtimeEvolution);
+    }
+
+    const demandsByProject = $("#demands-by-project");
+    if (demandsByProject.length !== 0) {
+        buildColumnChart(demandsByProject);
+    }
+
+    let donutDemandTypeDiv = $("#demands-type-donut");
+    if (donutDemandTypeDiv.length !== 0) {
+        buildDonutChart(donutDemandTypeDiv);
+    }
+
+    let donutDemandDeliveredDiv = $("#demands-delivered-donut");
+    if (donutDemandDeliveredDiv.length !== 0) {
+        buildDonutChart(donutDemandDeliveredDiv);
+    }
+
+    const leadtimeControlChart = $("#leadtime-control-chart");
+    if (leadtimeControlChart.length !== 0) {
+        buildScatterChart(leadtimeControlChart);
+    }
+
+    const columnDemandLeadTimeBreakdownDiv = $("#portfolio-unit-lead-time-breakdown-column");
+    if (columnDemandLeadTimeBreakdownDiv.length !== 0) {
+        buildColumnChart(columnDemandLeadTimeBreakdownDiv);
+    }
+};
