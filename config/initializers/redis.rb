bc969c3b9a9680e501dd62fe1358ef2b003eb8dd
Celso Martins <celso.av.martins@gmail.com>
2022-09-01 11:42:39 -0300

    chore: fixing SSL redis

--- config/initializers/redis.rb
+++
@@ -1,5 +0,0 @@
-# frozen_string_literal: true
-
-# rubocop:disable Style/GlobalVars
-$redis = Redis.new(url: ENV.fetch('REDIS_URL', nil), ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE })
-# rubocop:enable Style/GlobalVars
