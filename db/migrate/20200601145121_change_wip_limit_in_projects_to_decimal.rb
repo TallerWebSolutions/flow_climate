b22573af182e9bed5aa3f6d5a501b63400b69dd0
Luiz Henrique Guerra <lhguerra@gmail.com>
2022-09-22 11:35:47 -0300

    test: update specs

--- db/migrate/20200601145121_change_wip_limit_in_projects_to_decimal.rb
+++ db/migrate/20200601145121_change_wip_limit_in_projects_to_decimal.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+##
+# Wip limit should be decimal to improve monte carlo simulations.
 class ChangeWipLimitInProjectsToDecimal < ActiveRecord::Migration[6.0]
   def up
     change_column :projects, :max_work_in_progress, :decimal, default: 1
