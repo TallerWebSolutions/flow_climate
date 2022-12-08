b2c994d30b1343d26a00816c4ac6cac5b080fee9
Celso Martins <celso.av.martins@gmail.com>
2022-12-08 19:38:42 -0300

    chore: fixed spa lint

--- app/spa/src/modules/project/components/ProjectsTable.tsx
+++ app/spa/src/modules/project/components/ProjectsTable.tsx
@@ -1,5 +1,5 @@
 import { gql, useQuery } from "@apollo/client"
-import { TFunction, useTranslation } from "react-i18next"
+import { useTranslation } from "react-i18next"
 import {
   Backdrop,
   CircularProgress,
