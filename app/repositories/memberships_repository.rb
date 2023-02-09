f5ce33c0f9e3587ec0918d168ee645717226c42c
otarampinelli <otaviorampinellielias@gmail.com>
2023-02-09 14:19:38 -0300

    fix: fixed bug in blocking rating notification

--- app/repositories/memberships_repository.rb
+++ app/repositories/memberships_repository.rb
@@ -4,7 +4,7 @@ class MembershipsRepository
   include Singleton
 
   def find_or_create_by_name(team, member_name, member_role = :developer, start_date = Time.zone.today)
-    team_member = TeamMember.where(company: team.company).where('lower(name) LIKE :author_name', author_name: "%#{member_name.downcase}%").first_or_initialize
+    team_member = TeamMember.where(company: team.company).where('lower(name) LIKE :author_name', author_name: "#{member_name.downcase}").first_or_initialize
     team_member.update(start_date: start_date, name: member_name) unless team_member.persisted?
     membership = Membership.where(team: team, team_member: team_member).first_or_initialize
     membership.update(member_role: member_role, start_date: start_date) unless membership.persisted?
