7c322cfc4fffda26bfc73b58d064b93a748a1c49
daiTaller <daiane@taller.net.br>
2023-03-15 16:48:20 -0300

    chore: Corrected  lint

--- app/graphql/mutations/create_product_risk_review.rb
+++ app/graphql/mutations/create_product_risk_review.rb
@@ -15,7 +15,7 @@ module Mutations
     def resolve(company_id:, product_id:, lead_time_outlier_limit:, meeting_date:)
       product = Product.friendly.find_by(id: product_id)
       risk_review = RiskReview.create(company_id: company_id, product_id: product_id, lead_time_outlier_limit: lead_time_outlier_limit, meeting_date: meeting_date)
-      
+
       risk_review_url = company_product_risk_review_path(product.company, product, risk_review) if product.present?
 
       if risk_review.valid?
